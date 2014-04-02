d374a5d7c191dec81cae5bcf394945e4c9b15039
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-04-02 09:52:47 -0400

    Updated to reflect new data directory

--- seqio.py
+++ seqio.py
@@ -32,7 +32,7 @@ def fast_read_fasta(file_handle):
   Inputs:
     file_handle       - an open file handle
 
-  >>> header, seq = fast_read_fasta(open('porcine_circovirus.fa','r'))
+  >>> header, seq = fast_read_fasta(open('Data/porcine_circovirus.fa','r'))
   >>> header
   'gi|52547303|gb|AY735451.1| Porcine circovirus isolate Hebei capsid protein gene, complete cds'
   >>> seq[-20:]
@@ -61,11 +61,12 @@ def time_test():
   """Runs the fast_read_fasta function to compare it against BioPython."""
   import timeit
 
-  tm = timeit.Timer(stmt="seq = SeqIO.read('porcine_circovirus.fa', 'fasta')", setup='from Bio import SeqIO')
+  tm = timeit.Timer(stmt="seq = SeqIO.read('Data/porcine_circovirus.fa', 'fasta')", setup='from Bio import SeqIO')
   print 'BioPython'
   print tm.timeit(number=2000)
 
-  tm = timeit.Timer(stmt="header, seq = seqio.fast_read_fasta(open('porcine_circovirus.fa'))", setup='import seqio')
+  tm = timeit.Timer(stmt="header, seq = seqio.fast_read_fasta(open('Data/porcine_circovirus.fa'))",
+                    setup='import seqio')
   print 'fast_read_fasta'
   print tm.timeit(number=2000)
 
@@ -79,9 +80,10 @@ def write_test():
 
   """
   from Bio import SeqIO
-  header, seq = fast_read_fasta(open('porcine_circovirus.fa'))
+
+  header, seq = fast_read_fasta(open('Data/porcine_circovirus.fa'))
   fast_write_fasta(open('test.fa','w'), seq, header, width=70)
-  seq_orig = SeqIO.read('porcine_circovirus.fa','fasta')
+  seq_orig = SeqIO.read('Data/porcine_circovirus.fa', 'fasta')
   seq_written = SeqIO.read('test.fa','fasta')
   #print seq_orig.seq[-20:]
   print seq_written.seq[-20:]
