d2a32b01c071222e25ea2adb2f66c4787a854af0
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-04-04 16:04:42 -0400

    Updated parameter file, works with stock SNP plugin

--- Params/example_mutation_parameter_file.py
+++ Params/example_mutation_parameter_file.py
@@ -3,11 +3,19 @@
 Seven Bridges Genomics
 Current contact: kaushik.ghose@sbgenomics.com
 """
-mutation_program = {
-  'type': 'SNP',
-  ''
-}
 
-snp = {
-  'p': 0.01
+chrom = 1  # Chromosome number (VCF file requires this and a simulation might want to use this info)
+ref_file = 'Data/adenovirus.smalla'  # Reference sequence in smalla format
+out_file = 'Data/variants.vcf'  # VCF file name
+models = {
+  'snp': {
+    'model': 'snp',  # Stock SNP model
+    'start': 1000,  # Where on the seq to start generating SNPs
+    'stop': 5000,  # Where on the seq to end generating SNPs
+    'args': {
+      'p': 0.001,  # Stock SNP model has only one parameter
+      'poisson_rng_seed': 1,
+      'base_sub_rng_seed': 1  # Stock SNP model has two rngs that it needs seeds for
+    }
+  }
 }
