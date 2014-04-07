bed689f41a73f618ba084ea284b19ec2e9f3686e
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-04-07 17:05:08 -0400

    Complete working example with annotation.

---
+++ Recipes/complete_example.sh
@@ -0,0 +1,55 @@
+# A complete example that takes you from the sample fasta data files (and sample parameter files) to an
+# Aligned BAM that you can inspect
+
+set -x
+
+echo 'Convert fasta to smalla format using converta.py'
+python converta.py Data/porcine_circovirus.fa Data/porcine_circovirus.smalla
+
+echo 'Generate mutations in a VCF file'
+python mutate.py --paramfile=Params/example_mutation_parameter_file.json
+pushd Data
+cat variants.vcf
+cat variants.vcf.info
+popd
+
+pushd Data
+echo 'Use samtools to compress and index the variant file'
+bgzip -c variants.vcf > variants.vcf.gz
+tabix -p vcf variants.vcf.gz
+popd
+
+
+echo 'Use vcf2seq to generate mutated sequence from VCF and reference sequence'
+python vcf2seq.py Data/porcine_circovirus.smalla Data/mutated.smalla 1 Data/variants.vcf.gz
+
+
+echo 'Use reads to generate a bam file full of reads'
+python reads.py  --paramfile=Params/example_reads_parameter_file.json
+
+
+echo 'Use cheata to fake align the reads according to the coordinates we store in the seq id'
+pushd Data
+samtools sort corrupted_reads.bam sorted_corrupted_reads
+samtools index sorted_corrupted_reads.bam
+popd
+
+python cheata.py --inbam=Data/sorted_corrupted_reads.bam  --outbam=Data/cheat_alignment.bam
+
+echo 'Use samtools to index this fake alignment'
+pushd Data
+samtools sort cheat_alignment.bam sorted_cheat_alignment
+samtools index sorted_cheat_alignment.bam
+popd
+
+
+echo 'Use samtools to create a real alignment'
+pushd Data
+bwa index porcine_circovirus.fa
+samtools bam2fq corrupted_reads.bam > raw_reads.fq
+bwa mem porcine_circovirus.fa raw_reads.fq > aligned.sam
+samtools view -Sb aligned.sam > aligned.bam
+samtools sort aligned.bam aligned_sorted
+samtools index aligned_sorted.bam
+popd
+
