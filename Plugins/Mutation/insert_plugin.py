f1dfb2e1ffa655a69af13a08718555d2f97c40e5
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-04-24 11:29:10 -0400

    insert_plugin.py - added tests.

--- Plugins/Mutation/insert_plugin.py
+++ Plugins/Mutation/insert_plugin.py
@@ -21,10 +21,10 @@ def variant(p_ins=0.01, lam_ins=5,
   """A generator which returns a variant when asked for. This is the stock SNP generator and returns snp locations in
   a poisson distributed fashion.
   Inputs:
-    ref_seq              - The reference sequence
-    ref_seq_len          - length of whole sequence (needed to compute start and stop)
     p_ins                - probability of inserts
     lam_ins              - mean length of poisson distributed insert lengths
+    ref_seq              - The reference sequence
+    ref_seq_len          - length of whole sequence (needed to compute start and stop)
     start_ins_frac       - start generating inserts from here (0.0, 1.0)
     stop_ins_frac        - stop generating inserts after this (0.0, 1.0) stop_ins_frac > start_ins_frac
     ins_loc_rng_seed     - INS locator rng -> numpy.random.RandomState(seed)
@@ -37,18 +37,35 @@ def variant(p_ins=0.01, lam_ins=5,
     variant              - (POS, REF, ALT, skip, list(footprints))
 
 
+  Test with 'N' regions. There should be no insertions after an 'N'
+  >>> args = {'p_ins': .1, 'lam_ins': 3, 'ins_loc_rng_seed': 1, 'ins_len_rng_seed': 2, 'base_sel_rng_seed': 3}; \
+  ref_seq='ACGTACGTANGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGT'; ref_seq_len = len(ref_seq); \
+  gen = variant(ref_seq=ref_seq, ref_seq_len=ref_seq_len, block_size=100, **args);
+  >>> for n in range(10): print next(gen,None)
+  (15, 'T', 'TGAC', 16, None)
+  (22, 'G', 'GGG', 23, None)
+  (31, 'T', 'TGAA', 32, None)
+  (40, 'A', 'ACTCA', 41, None)
+  (49, 'C', 'CAT', 50, None)
+  (55, 'T', 'TA', 56, None)
+  None
+  None
+  None
+  None
+
+
   Test with one block
-  >>> args = {'p_del': .1, 'lam_del': 3, 'del_loc_rng_seed': 1, 'del_len_rng_seed': 2}; \
+  >>> args = {'p_ins': .1, 'lam_ins': 3, 'ins_loc_rng_seed': 1, 'ins_len_rng_seed': 2, 'base_sel_rng_seed': 3}; \
   ref_seq='ACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGT'; ref_seq_len = len(ref_seq); \
   gen = variant(ref_seq=ref_seq, ref_seq_len=ref_seq_len, block_size=100, **args);
   >>> for n in range(10): print next(gen,None)
-  (9, 'CG', 'C', 11, None)
-  (15, 'TACG', 'T', 19, None)
-  (22, 'GTA', 'G', 25, None)
-  (31, 'TACG', 'T', 35, None)
-  (40, 'ACGTA', 'A', 45, None)
-  (49, 'CGT', 'C', 52, None)
-  (55, 'TA', 'T', 57, None)
+  (9, 'C', 'CT', 10, None)
+  (15, 'T', 'TGAC', 16, None)
+  (22, 'G', 'GGG', 23, None)
+  (31, 'T', 'TGAA', 32, None)
+  (40, 'A', 'ACTCA', 41, None)
+  (49, 'C', 'CAT', 50, None)
+  (55, 'T', 'TA', 56, None)
   None
   None
   None
@@ -56,13 +73,13 @@ def variant(p_ins=0.01, lam_ins=5,
   Test with multiple blocks - should be same answer even though we have changed the block size
   >>> gen = variant(ref_seq=ref_seq, ref_seq_len=ref_seq_len, block_size=1, **args);
   >>> for n in range(10): print next(gen,None)
-  (9, 'CG', 'C', 11, None)
-  (15, 'TACG', 'T', 19, None)
-  (22, 'GTA', 'G', 25, None)
-  (31, 'TACG', 'T', 35, None)
-  (40, 'ACGTA', 'A', 45, None)
-  (49, 'CGT', 'C', 52, None)
-  (55, 'TA', 'T', 57, None)
+  (9, 'C', 'CT', 10, None)
+  (15, 'T', 'TGAC', 16, None)
+  (22, 'G', 'GGG', 23, None)
+  (31, 'T', 'TGAA', 32, None)
+  (40, 'A', 'ACTCA', 41, None)
+  (49, 'C', 'CAT', 50, None)
+  (55, 'T', 'TA', 56, None)
   None
   None
   None
@@ -88,7 +105,7 @@ def variant(p_ins=0.01, lam_ins=5,
   while locs[internal_cntr] < ins_end:
     vl = locs[internal_cntr]
     ref = ref_seq[vl]
-    if 'N' in ref:
+    if ref == 'N':
       alt = None  # Don't do insertions is regions with Ns
     else:
       alt = ref + ''.join([bases[k] for k in base_calls[internal_cntr]])
