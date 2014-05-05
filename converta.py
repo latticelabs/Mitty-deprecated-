abf4d7d9dd86bd8df4f9dd8f08cd5e53c913699f
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-05-05 12:23:17 -0400

    Housekeeping - rolling seqio routines into converta

--- converta.py
+++ converta.py
@@ -12,23 +12,66 @@ Seven Bridges Genomics
 Current contact: kaushik.ghose@sbgenomics.com
 """
 import docopt
-import seqio
-
 __version__ = '0.1.0'
+
+def get_fasta_header(fname):
+  """Read FASTA header
+
+  >>> print get_fasta_header(fname='Data/porcine_circovirus.fa')
+  gi|52547303|gb|AY735451.1| Porcine circovirus isolate Hebei capsid protein gene, complete cds
+  """
+  line = open(fname, 'r').readline().strip()
+  if len(line): line = line[1:]  # get rid of the leading >
+  return line
+
+
+def block_read_fasta(fname, block_size=1000):
+  """Read FASTA file in blocks. For now this simply discards any lines begining with '>'
+
+  >>> print ''.join([s for s in block_read_fasta(fname='Data/porcine_circovirus.fa', block_size=1)])
+  ATGACGTATCCAAGGAGGCGTTACCGGAGAAGAAGACACCGCCCCCGCAGCCATCTTGGCCAGATCCTCCGCCGCCGCCCCTGGCTCGTCCACCCCCGCCACCGTTACCGCTGGAGAAGGAAAAACGGCATCTTCAACACCCGCCTCTCCCGCACCTTCGGATATACTATCAAGCGAACCACAGTCAAAACGCCCTCCTGGGCGGTGGACATGATGAGATTCAATATTAATGACTTTCTTCCCCCAGGAGGGGGCTCAAACCCCCGCTCTGTGCCCTTTGAATACTACAGAATAAGAAAGGTTAAGGTTGAATTCTGGCCCTGCTCCCCGATCACCCAGGGTGACAGGGGAGTGGGCTCCAGTGCTGTTATTCTAGATGATAACTTTGTAACAAAGGCCACAGCCCTCACCTATGACCCCTATGTAAACTACTCCTCCCGCCATACCATAACCCAGCCCTTCTCCTACCACTCCCGCTACTTTACCCCCAAACCTGTCCTAGATTCCACTATTGATTACTTCCAACCAAACAACAAAAGAAATCAGCTGTGGCTGAGACTACAAACTGCTGGAAATGTAGACCACGTAGGCCTCGGCACTGCGTTCGAAAACAGTATATACGACCAGGAATACAATATCCGTGTAACCATGTATGTACAATTCAGAGAATTTAATCTTAAAGACCCCCCACTTAACCCTTAG
+  >>> print ''.join([s for s in block_read_fasta(fname='Data/porcine_circovirus.fa', block_size=13)])
+  ATGACGTATCCAAGGAGGCGTTACCGGAGAAGAAGACACCGCCCCCGCAGCCATCTTGGCCAGATCCTCCGCCGCCGCCCCTGGCTCGTCCACCCCCGCCACCGTTACCGCTGGAGAAGGAAAAACGGCATCTTCAACACCCGCCTCTCCCGCACCTTCGGATATACTATCAAGCGAACCACAGTCAAAACGCCCTCCTGGGCGGTGGACATGATGAGATTCAATATTAATGACTTTCTTCCCCCAGGAGGGGGCTCAAACCCCCGCTCTGTGCCCTTTGAATACTACAGAATAAGAAAGGTTAAGGTTGAATTCTGGCCCTGCTCCCCGATCACCCAGGGTGACAGGGGAGTGGGCTCCAGTGCTGTTATTCTAGATGATAACTTTGTAACAAAGGCCACAGCCCTCACCTATGACCCCTATGTAAACTACTCCTCCCGCCATACCATAACCCAGCCCTTCTCCTACCACTCCCGCTACTTTACCCCCAAACCTGTCCTAGATTCCACTATTGATTACTTCCAACCAAACAACAAAAGAAATCAGCTGTGGCTGAGACTACAAACTGCTGGAAATGTAGACCACGTAGGCCTCGGCACTGCGTTCGAAAACAGTATATACGACCAGGAATACAATATCCGTGTAACCATGTATGTACAATTCAGAGAATTTAATCTTAAAGACCCCCCACTTAACCCTTAG
+  >>> print ''.join([s for s in block_read_fasta(fname='Data/porcine_circovirus.fa', block_size=1000)])
+  ATGACGTATCCAAGGAGGCGTTACCGGAGAAGAAGACACCGCCCCCGCAGCCATCTTGGCCAGATCCTCCGCCGCCGCCCCTGGCTCGTCCACCCCCGCCACCGTTACCGCTGGAGAAGGAAAAACGGCATCTTCAACACCCGCCTCTCCCGCACCTTCGGATATACTATCAAGCGAACCACAGTCAAAACGCCCTCCTGGGCGGTGGACATGATGAGATTCAATATTAATGACTTTCTTCCCCCAGGAGGGGGCTCAAACCCCCGCTCTGTGCCCTTTGAATACTACAGAATAAGAAAGGTTAAGGTTGAATTCTGGCCCTGCTCCCCGATCACCCAGGGTGACAGGGGAGTGGGCTCCAGTGCTGTTATTCTAGATGATAACTTTGTAACAAAGGCCACAGCCCTCACCTATGACCCCTATGTAAACTACTCCTCCCGCCATACCATAACCCAGCCCTTCTCCTACCACTCCCGCTACTTTACCCCCAAACCTGTCCTAGATTCCACTATTGATTACTTCCAACCAAACAACAAAAGAAATCAGCTGTGGCTGAGACTACAAACTGCTGGAAATGTAGACCACGTAGGCCTCGGCACTGCGTTCGAAAACAGTATATACGACCAGGAATACAATATCCGTGTAACCATGTATGTACAATTCAGAGAATTTAATCTTAAAGACCCCCCACTTAACCCTTAG
+  """
+  seq_buff = ''
+  seq_buff_len = 0
+  with open(fname, 'r') as f:
+    for line in f.readlines():
+      if line[0] == '>':
+
+        continue  # A seq id line
+      line = line.strip()
+      seq_buff_len += len(line)
+      seq_buff += line
+      while seq_buff_len > block_size:  # Time to spit it out
+        seq = seq_buff[:block_size]
+        seq_buff = seq_buff[block_size:]
+        seq_buff_len -= block_size
+        yield seq
+
+    if seq_buff_len:
+      yield seq_buff  # The last tail after the file ends
 
 
 def fasta_to_smalla(fasta_fname, smalla_fname, block_size=1000):
   with open(smalla_fname + '.heada', 'w') as f:
-    f.write(seqio.get_fasta_header(args['<fasta>']))
+    f.write(get_fasta_header(args['<fasta>']))
 
   with open(smalla_fname, 'w') as f:
-    for s in seqio.block_read_fasta(fname=fasta_fname, block_size=block_size):
+    for s in block_read_fasta(fname=fasta_fname, block_size=block_size):
       f.write(s)
 
 
 if __name__ == "__main__":
   if len(docopt.sys.argv) < 2:  # Print help message if no options are passed
     docopt.docopt(__doc__, ['-h'])
+  elif docopt.sys.argv[1] == 'test':
+    import sys
+    import doctest
+    doctest.testmod()
+    sys.exit()
   else:
     args = docopt.docopt(__doc__, version=__version__)
   fasta_to_smalla(args['<fasta>'], args['<smalla>'], int(args['--block_size']))
