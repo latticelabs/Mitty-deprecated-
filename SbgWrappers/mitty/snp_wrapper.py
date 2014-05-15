4e8f28412e47ba5b9c82325b024c646223df161d
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-05-15 14:51:06 -0400

    snp_wrapper.py - updated docs, using 'OUTPUT' directory and writing .meta information

--- SbgWrappers/mitty/snp_wrapper.py
+++ SbgWrappers/mitty/snp_wrapper.py
@@ -3,22 +3,24 @@
 
 The .json fragment should look like
 
-    "snp": {
-        "model": "snp",
-        "start_snps_frac": 0.1,
-        "stop_snps_frac":  0.3,
-        "phet": 0,
-        "p": 0.01,
-        "poisson_rng_seed": 1,
-        "base_sub_rng_seed": 2
-    },
+      "snp": {
+          "model": "snp",
+          "start_snps_frac": 0.1,
+          "stop_snps_frac":  0.3,
+          "phet": 0.5,
+          "p": 0.01,
+          "het_rng_seed":3,
+          "strand_rng_seed": 4,
+          "poisson_rng_seed": 1,
+          "base_sub_rng_seed": 2
+      }
 
 mutate_wrapper will place this snippet under "mutations"
 """
-from sbgsdk import define, Process, require
+import os
 import json
+from sbgsdk import define
 from nose.tools import assert_equals
-
 
 class SNP(define.Wrapper):
   class Inputs(define.Inputs):
@@ -26,39 +28,49 @@ class SNP(define.Wrapper):
     # ref = define.input(name='Reference', description='The reference file')
 
   class Outputs(define.Outputs):
-    json_fragment = define.output()
+    json_fragment = define.output(name="SNPs", description="SNP definitions for mutate")
 
   class Params(define.Params):
-    model_id = define.string(required=True)   # "snp" in the example above.
-                                              # Needs to be unique8, currently not used by mutate
-    start_snps_frac = define.real(default=0, min=0, max=1, category='SNP model')
-    stop_snps_frac = define.real(default=1, min=0, max=1, category='SNP model')
-    phet = define.real(default=0.01, min=0, max=1, category='SNP model')
-    p = define.real(default=0.01, min=0, max=1, category='SNP model')
-    poisson_rng_seed = define.integer(default=1, min=0, category='SNP model')
-    base_sub_rng_seed = define.integer(default=1, min=0, category='SNP model')
-
-    # def make_json(self):
-    #   params = self.__json__()
-    #   params.pop('model_id')
-    #   return {self.model_id: dict(model='snp', **params)}
+    model_id = define.string(required=True, description='A unique name for this instance of the SNP generator')
+    # "snp" in the example above. Needs to be unique8, currently not used by mutate
+    start_snps_frac = define.real(default=0, min=0, max=1, category='SNP model',
+                                  description='start generating snps from here (0.0, 1.0)')
+    stop_snps_frac = define.real(default=1, min=0, max=1, category='SNP model',
+                                 description='stop generating snps after this (0.0, 1.0) stop_snps_frac > start_snps_frac')
+    phet = define.real(default=0.01, min=0, max=1, category='SNP model',
+                       description='probability of having heterozygous mutation')
+    p = define.real(default=0.01, min=0, max=1, category='SNP model',
+                    description='probability of SNPs')
+    het_rng_seed = define.integer(default=1, min=0, max=2**32 - 1, category='SNP model',
+          description='Seed for random number generator used to decide if genotype is heterozygous or not')
+    strand_rng_seed = define.integer(default=1, min=0, max=2**32 - 1, category='SNP model',
+          description='Seed for random number generator used to decide which strand the SNP will be on')
+    poisson_rng_seed = define.integer(default=1, min=0, max=2**32 - 1, category='SNP model',
+          description='Seed for SNP locator random number generator')
+    base_sub_rng_seed = define.integer(default=1, min=0, max=2**32 - 1, category='SNP model',
+          description='Seed for random number generator used to select ALT bases')
 
   def execute(self):
-    self.outputs.json_fragment = 'snp_plugin_params.json'
+    output_dir = 'OUTPUT'
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+    self.outputs.json_fragment = os.path.join(output_dir, 'snp_plugin_params.json')
     with open(self.outputs.json_fragment, 'w') as f:
       params = self.params.__json__()
       params.pop('model_id')
       json.dump({self.params.model_id: dict(model='snp', **params)}, f)
-      #json.dump(self.params.make_json(), f)
+    self.outputs.json_fragment.meta = self.outputs.json_fragment.make_metadata(file_type='json')
 
 
 def test():
   params = {
             "model_id": "snp_test",
             "start_snps_frac": 0.1,
-            "stop_snps_frac":  0.3,
+            "stop_snps_frac": 0.3,
             "phet": 0,
             "p": 0.01,
+            "het_rng_seed": 3,
+            "strand_rng_seed": 4,
             "poisson_rng_seed": 1,
             "base_sub_rng_seed": 2
         }
@@ -71,9 +83,10 @@ def test():
             "stop_snps_frac":  0.3,
             "phet": 0,
             "p": 0.01,
+            "het_rng_seed":3,
+            "strand_rng_seed": 4,
             "poisson_rng_seed": 1,
             "base_sub_rng_seed": 2
         }}
   with open(outputs.json_fragment) as fp:
     assert_equals(json.load(fp), expected)
-  #assert os.path.getsize('snp_plugin_params.json')
