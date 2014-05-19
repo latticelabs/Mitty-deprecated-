4d5b2def531e4603b8de2d22ff8d7e4ef0c8cfeb
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-05-19 17:02:51 -0400

    __init__.py - adding new wrappers

--- SbgWrappers/mitty/__init__.py
+++ SbgWrappers/mitty/__init__.py
@@ -1,2 +1,5 @@
-from .snp_wrapper import SNP 
 from .mutate_wrapper import Mutate
+from .vcf2seq_wrapper import Vcf2Seq
+from .plugins.mutation.snp_wrapper import SNP
+from .plugins.mutation.insert_wrapper import Insert
+from .plugins.mutation.delete_wrapper import Deletion
