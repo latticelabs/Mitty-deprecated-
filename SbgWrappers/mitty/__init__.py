827906619869b6ae00586f8801db7a124b868f91
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-05-22 10:48:13 -0400

    added wrappers for reads and cheata

--- SbgWrappers/mitty/__init__.py
+++ SbgWrappers/mitty/__init__.py
@@ -1,6 +1,8 @@
 from .converta_wrapper import Converta
 from .mutate_wrapper import Mutate
 from .vcf2seq_wrapper import Vcf2Seq
+from .reads_wrapper import Reads
+from .cheata_wrapper import Cheata
 
 from .plugins.mutation.snp_wrapper import SNP
 from .plugins.mutation.insert_wrapper import Insert
