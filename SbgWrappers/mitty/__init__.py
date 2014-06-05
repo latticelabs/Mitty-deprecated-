3b57b41aa77054bf4f12115f06f353e451bb9595
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-06-05 13:25:41 -0400

    Adding checka to the list

--- SbgWrappers/mitty/__init__.py
+++ SbgWrappers/mitty/__init__.py
@@ -3,6 +3,7 @@ from .mutate_wrapper import Mutate
 from .vcf2seq_wrapper import Vcf2Seq
 from .reads_wrapper import Reads
 from .cheata_wrapper import Cheata
+from .checka_wrapper import Checka
 
 from .plugins.mutation.snp_wrapper import SNP
 from .plugins.mutation.insert_wrapper import Insert
