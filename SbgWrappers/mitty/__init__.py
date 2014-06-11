4306378f1ca394799b2662a6e82f15f677eb42df
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-06-11 13:25:16 -0400

    Updated with new wrappers

--- SbgWrappers/mitty/__init__.py
+++ SbgWrappers/mitty/__init__.py
@@ -3,10 +3,12 @@ from .mutate_wrapper import Mutate
 from .vcf2seq_wrapper import Vcf2Seq
 from .reads_wrapper import Reads
 from .cheata_wrapper import Cheata
-from .checka_wrapper import Checka
+from .cheata_split_wrapper import SplitGoodBadAlignments
 
 from .plugins.mutation.snp_wrapper import SNP
 from .plugins.mutation.insert_wrapper import Insert
 from .plugins.mutation.delete_wrapper import Deletion
 
 from .plugins.reads.simple_reads_wrapper import SimpleReads
+
+from .analysis.diagnose_alignment_wrapper import PlotAlign
