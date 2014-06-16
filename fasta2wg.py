0b3c504b8f481211c0a0050c73799c98050eb1c2
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-06-16 17:30:34 -0400

    New version of whole genome concatenator

--- fasta2wg.py
+++ fasta2wg.py
@@ -2,18 +2,17 @@
 released by the GRC into a .wg.gz file.
 
 Usage:
-fasta2wg  --fa=FA  --wg=WG  --chrom_count=CC  [--species=SP] [-v]
+fasta2wg  --index=IDX  --wg=WG  [-v]
 
 Options:
-  --fa=FA           Input .fa.gz file
+  --index=IDX       Index file (.json) listing fasta files to be inserted into whole genome
   --wg=WG           Name of .wg.gz file to save to (Saved as gzipped .wg file)
-  --chrom_count=CC  How many chromosomes in the file
-  --species=SP      What string would we like at appear in the whole genome file [default: Test]
   -v                Be verbose when you do things
 
 Seven Bridges Genomics
 Current contact: kaushik.ghose@sbgenomics.com
 """
+import json
 import gzip
 import docopt
 import genome
@@ -23,28 +22,12 @@ logger = logging.getLogger(__name__)
 __version__ = '0.2.0'
 
 
-def fasta_read_generator(fasta_fname):
-  """Given a fasta filename return a generator that yields us sequences and sequence ids from the file"""
+def read_single_seq_fasta(fasta_fname):
+  """Given a fasta filename with one sequence read it."""
   with gzip.open(fasta_fname, 'rb') as fasta_fp:
-    read_a_byte = fasta_fp.read
-    reading_a_seq = False
-    seq_id = None
-    seq = []
-    while True:
-      byte = read_a_byte(1)
-      if not byte or byte == '>': # Either file is done, or a new seq starting
-        if reading_a_seq:
-          fasta_fp.seek(-1, 1)  # Pretend this never happened next time round
-          reading_a_seq = False
-          yield seq_id, ''.join(seq)
-        if byte == '>':  # New sequence
-          reading_a_seq = True
-          seq_id = fasta_fp.readline()[:-1]
-          seq = []
-        if not byte:  # Done with file
-          break
-      elif byte != '\n':  # Part of the sequence
-        seq += byte.upper()
+    seq_id = fasta_fp.readline()[1:-1]
+    seq = fasta_fp.read().replace('\n','').upper()
+  return seq_id, seq
 
 if __name__ == "__main__":
   if len(docopt.sys.argv) < 2:  # Print help message if no options are passed
@@ -55,8 +38,12 @@ if __name__ == "__main__":
   level = logging.DEBUG if args['-v'] else logging.WARNING
   logging.basicConfig(level=level)
 
-  with genome.WholeGenome(fname=args['--wg'], species=args['--species'],
-                          chrom_count=int(args['--chrom_count'])) as wg:
-    for n, (this_seq_id, this_seq) in enumerate(fasta_read_generator(args['--fa'])):
-      if wg.insert_seq(this_seq, chrom_no=n + 1, chrom_cpy=1, seq_id=this_seq_id):
-        print 'Inserted chromosome {:d}'.format(n + 1)
+  idx = json.load(open(args['--index'],'r'))
+  with genome.WholeGenome(fname=args['--wg'], species=idx['header']['species'].encode('ascii'),
+                          chrom_count=idx['header']['chromosome count']) as wg:
+    for k, fasta_fname in idx['chromosomes'].iteritems():
+
+      chrom_no, chrom_cpy = int(k.split(':')[0]), int(k.split(':')[1])
+      this_seq_id, this_seq = read_single_seq_fasta(fasta_fname)
+      if wg.insert_seq(this_seq, chrom_no=chrom_no, chrom_cpy=chrom_cpy, seq_id=this_seq_id):
+        print 'Inserted chromosome {:d}, copy {:d} ({:s})'.format(chrom_no, chrom_cpy, this_seq_id)
