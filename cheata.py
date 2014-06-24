d9edb295eb0ea6bd156c6910b94068271ea26d74
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-06-24 19:02:11 -0400

    Now saving comprehensive read analysis file

--- cheata.py
+++ cheata.py
@@ -22,12 +22,15 @@ Notes:
 2. As of v 0.2.0 there is a quirk with pysam writing BAM files where alignments have some hidden problem when written as
    BAM from cheata. My work around is to save as SAM and then convert to BAM which seems to not create any problems.
 3. The `split` mode enables us to break the larger aligned .bam file into smaller files. Since we are often interested
-   in just the errors, we can look at just the _wrong.bam which is often much smaller.
+   in just the errors, we can look at just the _wrong.bam which is often much smaller. We also get a summary of the
+   alignment performance for easy plotting.
 """
 __version__ = '0.2.0'
 
 import os  # Needed for filesize (for sequence length) and messing with filenames
 import pysam  # Needed to read/write BAM files
+import numpy
+import cPickle
 import docopt
 import genome
 import reads
@@ -76,39 +79,66 @@ def align(inbam, outbam, seq_name, seq_len):
   sort_and_index_bam(outbam)
 
 
-def split(wg, in_bam, correct_bam, wrong_bam, unmapped_bam):
+def split(wg, in_bam, correct_bam, wrong_bam, unmapped_bam, data_save_fp):
   """Split the aligned BAM file into three sets of reads - properly aligned, misaligned and unmapped - and save them in
   separate bam files
   """
   #The rname in the aligned read corresponds to the sequence in the list in_bam.header['SQ']
   #We need to match that up with the chromosome code we have in the wg file
-  seq_name_list = [sq['SN'] for sq in in_bam.header['SQ']]
+  seq_name_list = in_bam.references
   rev_idx = wg.reverse_index
-  cnt = 0
-  blk = 0
-  good_cnt, bad_cnt, unmapped_cnt = 0, 0, 0
-  for read in in_bam:
+
+  total_reads = in_bam.mapped + in_bam.unmapped   #total_reads = int(pysam.view('-c', in_bam.filename)[0].strip())
+
+  #This array stores our parsed read data
+  # read_analysis = numpy.recarray(shape=(total_reads,),
+  #                                dtype=[('correct_chrom', '5S'), ('correct_pos', '<i4'),
+  #                                       ('correct_cigar', '255S'),
+  #                                       ('aligned_chrom', '5S'), ('aligned_pos', '<i4'),
+  #                                       ('aligned_cigar', '255S'),
+  #                                       ('mapped', bool), ('correctly_aligned', bool),
+  #                                       ('mapping_qal', '<i1')])
+
+  read_analysis = numpy.recarray(shape=(total_reads,),
+                                 dtype=[('correct_chrom', '5S'), ('correct_pos', '<i4'),
+                                        ('aligned_chrom', '5S'), ('aligned_pos', '<i4'),
+                                        ('mapped', bool), ('correctly_aligned', bool),
+                                        ('mapping_qual', '<i1')])
+
+  for n, read in enumerate(in_bam):
+    correct_chrom, correct_pos, correct_cigar = reads.interpret_read_qname(read)
+    read_analysis[n].correct_chrom = correct_chrom
+    read_analysis[n].correct_pos = correct_pos
+    #read_analysis[n].correct_cigar = correct_cigar
     if read.flag & 0x4:  # Unmapped
+      read_analysis[n].mapped = False
       unmapped_bam.write(read)
-      unmapped_cnt += 1
     else:
-      correct_chrom, correct_pos, correct_cigar = reads.interpret_read_qname(read)
+      read_analysis[n].mapped = True
       aligned_chrom = rev_idx[seq_name_list[read.rname]]
       aligned_pos = read.pos + 1  # PySam uses 0 indexing ...
+
+      read_analysis[n].aligned_chrom = aligned_chrom
+      read_analysis[n].aligned_pos = aligned_pos
+      #read_analysis[n].correct_cigar = correct_cigar
+      read_analysis[n].mapping_qual = read.mapq
+
       if correct_chrom == aligned_chrom and correct_pos == aligned_pos:
         correct_bam.write(read)
-        good_cnt += 1
+        read_analysis[n].correctly_aligned = True
       else:
         wrong_bam.write(read)
-        bad_cnt += 1
-    blk += 1
-    if blk == 100000:
-      cnt += blk
-      logger.debug('{:d} reads done'.format(cnt))
-      blk = 0
+        read_analysis[n].correctly_aligned = False
 
-  logger.debug('{:d} reads done'.format(cnt))
-  logger.debug('Found {:d} good reads, {:d} bad reads, {:d} unmapped reads'.format(good_cnt, bad_cnt, unmapped_cnt))
+  cPickle.dump({
+    'read data': read_analysis,
+    'reference data': {rev_idx[seq_name]: {'name': seq_name, 'len': l} for seq_name, l in zip(seq_name_list, in_bam.lengths)}
+  }, data_save_fp, protocol=cPickle.HIGHEST_PROTOCOL)
+
+  logger.debug('{:d} reads done'.format(total_reads))
+  good_cnt = numpy.count_nonzero(read_analysis.correctly_aligned)
+  bad_cnt = in_bam.mapped - good_cnt
+  logger.debug('Found {:d} good reads, {:d} bad reads, {:d} unmapped reads'.format(good_cnt, bad_cnt, in_bam.unmapped))
 
 
 if __name__ == "__main__":
@@ -125,12 +155,14 @@ if __name__ == "__main__":
     correct_bam_name = os.path.splitext(inbam_name)[0] + '_correct.bam'
     wrong_bam_name = os.path.splitext(inbam_name)[0] + '_wrong.bam'
     unmapped_bam_name = os.path.splitext(inbam_name)[0] + '_unmapped.bam'
+    data_file_name = os.path.splitext(inbam_name)[0] + '_alignment_data.pkl'
     with genome.WholeGenome(fname=args['--wg']) as wg:
       in_bam_f=pysam.Samfile(inbam_name, 'rb')
       split(wg, in_bam=in_bam_f,
             correct_bam=pysam.Samfile(correct_bam_name, 'wb', header=in_bam_f.header),
             wrong_bam=pysam.Samfile(wrong_bam_name, 'wb', header=in_bam_f.header),
-            unmapped_bam=pysam.Samfile(unmapped_bam_name, 'wb', header=in_bam_f.header))
+            unmapped_bam=pysam.Samfile(unmapped_bam_name, 'wb', header=in_bam_f.header),
+            data_save_fp=open(data_file_name, 'wb'))
   else:  # We are in align mode
     seq_name, seq_len = open(args['--heada']).readlines()
     align(args['--inbam'], args['--outbam'], seq_name, seq_len)
