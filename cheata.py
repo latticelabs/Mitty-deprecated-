49e167175b11ec8ff918ccd0aad4f4d6671ecba2
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-06-24 10:11:30 -0400

    split working with new format + added unmapped reads
    TODO: update cheat aligner

--- cheata.py
+++ cheata.py
@@ -1,29 +1,36 @@
-"""This script reads in a BAM file created by `reads.py` and aligns the reads by using the coordinates stored in the
-seq id string. This aligned file can be read in using a visualizer to debug the simulation chain. Using the `split`
-option treats the input file as an BAM produced by an aligner and splits the reads into two files: correctly aligned
-and incorrectly aligned (_correct.bam, _wrong.bam).
+"""In default mode, this script reads in a BAM file created by `reads.py` and aligns the reads by using the coordinates
+stored in the seq id string. This aligned file can be read in using a visualizer to debug the simulation chain.
+
+Using the `split` option treats the input file as an BAM produced by an aligner and splits the reads into
+correctly aligned, incorrectly aligned and unmapped (_correct.bam, _wrong.bam, _unmapped.bam).
 
 Usage:
 cheata  --inbam=INBAM  --outbam=OUTBAM  --heada=HD  [-v]
-cheata split --inbam=INBAM  [-v]
+cheata split --inbam=INBAM  --wg=WG [-v]
 
 Options:
   --inbam=INBAM           Input (unaligned) bam file name of reads from reads.py
   --outbam=OUTBAM         Output (perfectly aligned) bam file name
+  --wg=WG                 Whole genome file. Needed for the index, to match BAM seq ids to chrom_key in read qname
   --heada=HD              Heada file produced by converta. Contains sequence name and len. These are put into the
                           cheat aligned BAM file header as they are needed by some tools such as Tablet
   -v                      Dump detailed logger messages
 
 Notes:
-1. Recall that the seq id of each read is the string 'rN:POS1:CIGAR1:POS2:CIGAR2'. Unpaired reads have no POS2, CIGAR2
+1. Recall that the seq id of each read is the string 'chrom:copy:rN:POS1:CIGAR1:POS2:CIGAR2'.
+   Unpaired reads have no POS2, CIGAR2
 2. As of v 0.2.0 there is a quirk with pysam writing BAM files where alignments have some hidden problem when written as
-BAM from cheata. My work around is to save as SAM and then convert to BAM which seems to not create any problems.
+   BAM from cheata. My work around is to save as SAM and then convert to BAM which seems to not create any problems.
+3. The `split` mode enables us to break the larger aligned .bam file into smaller files. Since we are often interested
+   in just the errors, we can look at just the _wrong.bam which is often much smaller.
 """
 __version__ = '0.2.0'
 
 import os  # Needed for filesize (for sequence length) and messing with filenames
 import pysam  # Needed to read/write BAM files
 import docopt
+import genome
+import reads
 import logging
 logger = logging.getLogger(__name__)
 
@@ -36,6 +43,9 @@ def sort_and_index_bam(bamfile):
 
 
 def align(inbam, outbam, seq_name, seq_len):
+  logger.error('Not updated yet')
+  raise RuntimeError
+
   in_bamfile = pysam.Samfile(inbam, 'rb')
   out_hdr = in_bamfile.header
   out_hdr['SQ'][0]['SN'] = seq_name.split(' ')[0].strip()  # Some programs, like tablet, can't handle seq names with spaces
@@ -66,38 +76,31 @@ def align(inbam, outbam, seq_name, seq_len):
   sort_and_index_bam(outbam)
 
 
-def split_good_bad(inbam):
-  """Split the aligned BAM file into two sets of reads - properly aligned and misaligned - and save them in two bam
-  files"""
-
-  correct_bam = os.path.splitext(inbam)[0] + '_correct.bam'
-  wrong_bam = os.path.splitext(inbam)[0] + '_wrong.bam'
-
-  in_bamfile = pysam.Samfile(inbam, 'rb')
-
-  out_hdr = in_bamfile.header
-  correct_bamfile = pysam.Samfile(correct_bam, 'wb', header=out_hdr)
-  wrong_bamfile = pysam.Samfile(wrong_bam, 'wb', header=out_hdr)
-
+def split(wg, in_bam, correct_bam, wrong_bam, unmapped_bam):
+  """Split the aligned BAM file into three sets of reads - properly aligned, misaligned and unmapped - and save them in
+  separate bam files
+  """
+  #The rname in the aligned read corresponds to the sequence in the list in_bam.header['SQ']
+  #We need to match that up with the chromosome code we have in the wg file
+  seq_name_list = [sq['SN'] for sq in in_bam.header['SQ']]
+  rev_idx = wg.reverse_index
   cnt = 0
   blk = 0
-  good_cnt = 0
-  bad_cnt = 0
-  for read in in_bamfile:
-    cheat_answer = read.qname.split(':')
-    this_correct_pos = int(cheat_answer[1])
-    if read.flag & 0x01:  # Paired reads
-      if read.flag & 0x80:  # Second end (mate)
-        this_correct_pos = int(cheat_answer[3])
-
-    computed_pos = read.pos + 1  # PySam uses 0 indexing ...
-
-    if this_correct_pos != computed_pos:  # We go into the bad pile
-      wrong_bamfile.write(read)
-      bad_cnt += 1
+  good_cnt, bad_cnt, unmapped_cnt = 0, 0, 0
+  for read in in_bam:
+    if read.flag & 0x4:  # Unmapped
+      unmapped_bam.write(read)
+      unmapped_cnt += 1
     else:
-      correct_bamfile.write(read)
-      good_cnt += 1
+      correct_chrom, correct_pos, correct_cigar = reads.interpret_read_qname(read)
+      aligned_chrom = rev_idx[seq_name_list[read.rname]]
+      aligned_pos = read.pos + 1  # PySam uses 0 indexing ...
+      if correct_chrom == aligned_chrom and correct_pos == aligned_pos:
+        correct_bam.write(read)
+        good_cnt += 1
+      else:
+        wrong_bam.write(read)
+        bad_cnt += 1
     blk += 1
     if blk == 100000:
       cnt += blk
@@ -105,12 +108,7 @@ def split_good_bad(inbam):
       blk = 0
 
   logger.debug('{:d} reads done'.format(cnt))
-  logger.debug('Found {:d} good reads, {:d} bad reads'.format(good_cnt, bad_cnt))
-
-  correct_bamfile.close()
-  wrong_bamfile.close()
-  sort_and_index_bam(correct_bam)
-  sort_and_index_bam(wrong_bam)
+  logger.debug('Found {:d} good reads, {:d} bad reads, {:d} unmapped reads'.format(good_cnt, bad_cnt, unmapped_cnt))
 
 
 if __name__ == "__main__":
@@ -123,7 +121,16 @@ if __name__ == "__main__":
   logging.basicConfig(level=level)
 
   if args['split']:  # We are in split_good_bad mode
-    split_good_bad(args['--inbam'])
+    inbam_name = args['--inbam']
+    correct_bam_name = os.path.splitext(inbam_name)[0] + '_correct.bam'
+    wrong_bam_name = os.path.splitext(inbam_name)[0] + '_wrong.bam'
+    unmapped_bam_name = os.path.splitext(inbam_name)[0] + '_unmapped.bam'
+    with genome.WholeGenome(fname=args['--wg']) as wg:
+      in_bam_f=pysam.Samfile(inbam_name, 'rb')
+      split(wg, in_bam=in_bam_f,
+            correct_bam=pysam.Samfile(correct_bam_name, 'wb', header=in_bam_f.header),
+            wrong_bam=pysam.Samfile(wrong_bam_name, 'wb', header=in_bam_f.header),
+            unmapped_bam=pysam.Samfile(unmapped_bam_name, 'wb', header=in_bam_f.header))
   else:  # We are in align mode
     seq_name, seq_len = open(args['--heada']).readlines()
     align(args['--inbam'], args['--outbam'], seq_name, seq_len)
