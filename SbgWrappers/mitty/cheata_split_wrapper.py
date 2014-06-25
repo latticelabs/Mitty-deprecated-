8e03a767898e03754d8ac92f12e5b0b9c04b4ea4
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-06-24 22:00:08 -0400

    Now copying over bam and bai into same folder to ensure index is found.

--- SbgWrappers/mitty/cheata_split_wrapper.py
+++ SbgWrappers/mitty/cheata_split_wrapper.py
@@ -12,7 +12,8 @@ import os
 class SplitGoodBadAlignments(define.Wrapper):
   class Inputs(define.Inputs):
     bam = define.input(name='BAM', description='aligned .bam file of reads produced by read simulator', required=True)  # , file_types=['.bam'])
-    wg = define.input(name='wg.gz', description='Whole genome .gz file')
+    bai = define.input(name='BAI', description='index for aligned .bam file of reads produced by read simulator', required=True)
+    wg = define.input(name='wg.gz', description='Whole genome .gz file', required=True)
 
   class Outputs(define.Outputs):
     good_bam = define.output(name='good BAM', description='.bam (and index) file of correctly aligned reads')  #, list=True)  # , file_types=['.bam', '.bai'])
@@ -21,8 +22,21 @@ class SplitGoodBadAlignments(define.Wrapper):
     data_file = define.output(name='Data file (.pkl)', description='.pkl file of table of read analysis')
 
   def execute(self):
-    inbam = self.inputs.bam
+    # We need the index and the bam file to be in the same directory, and the only way to ensure this is to
+    # copy them ourselves
+    _inbam = self.inputs.bam
+    _inbam_bai = self.inputs.bai
+    working_dir = 'WDIR/'
+    if not os.path.exists(working_dir):
+      os.makedirs(working_dir)
+    p = Process('cp', _inbam, working_dir)
+    p.run()
+    p = Process('cp', _inbam_bai, working_dir)
+    p.run()
+
+    inbam = os.path.join(working_dir, os.path.basename(self.inputs.bam))
     wg = self.inputs.wg
+
     p = Process('python', '/Mitty/cheata.py', 'split', '--inbam', inbam, '--wg', wg, '-v')
     p.run()
 
@@ -31,25 +45,23 @@ class SplitGoodBadAlignments(define.Wrapper):
     unmapped_bam = os.path.splitext(inbam)[0] + '_unmapped.bam'
     data_file = os.path.splitext(inbam)[0] + '_alignment_data.pkl'
 
-    self.outputs.good_bam = correct_bam  #.add_file(correct_bam)
-    #self.outputs.good_bam.add_file(correct_bam + '.bai')
+    self.outputs.good_bam = correct_bam
     self.outputs.good_bam.meta = self.inputs.bam.make_metadata()
 
-    self.outputs.bad_bam = bad_bam  #.add_file(bad_bam)
-    #self.outputs.bad_bam.add_file(bad_bam + '.bai')
+    self.outputs.bad_bam = bad_bam
     self.outputs.bad_bam.meta = self.inputs.bam.make_metadata()
 
-    self.outputs.unmapped_bam = unmapped_bam  # .add_file(unmapped_bam)
-    #self.outputs.unmapped_bam.add_file(unmapped_bam + '.bai')
+    self.outputs.unmapped_bam = unmapped_bam
     self.outputs.unmapped_bam.meta = self.inputs.bam.make_metadata()
 
-    self.outputs.data_file = data_file  # .add_file(unmapped_bam)
+    self.outputs.data_file = data_file
 
 
 def test_checka():
   """Test with the porcine circovirus test data"""
 
   inputs = {'bam': '/sbgenomics/test-data/bwa_aligned_chimera.bam',
+            'bai': '/sbgenomics/test-data/bwa_aligned_chimera.bam.bai',
             'wg': '/sbgenomics/test-data/chimera.wg.gz'}
   params = {}
   wrp = SplitGoodBadAlignments(inputs, params)
