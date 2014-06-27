70d8b2ede0ce15ad7a21f4055da807cafa7cdf3b
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-06-27 13:50:13 -0400

    Added centromere positions

--- Analysis/plot_bad_alignments.py
+++ Analysis/plot_bad_alignments.py
@@ -14,6 +14,33 @@ import pylab
 import docopt
 import os
 
+centromere_pos = {
+  1: 125.0e6,
+  2: 93.3e6,
+  3: 91.0e6,
+  4: 50.4e6,
+  5: 48.4e6,
+  6: 61.0e6,
+  7: 59.9e6,
+  8: 45.6e6,
+  9: 49.0e6,
+  10: 40.2e6,
+  11: 53.7e6,
+  12: 35.8e6,
+  13: 17.9e6,
+  14: 17.6e6,
+  15: 19.0e6,
+  16: 36.6e6,
+  17: 24.0e6,
+  18: 17.2e6,
+  19:	26.5e6,
+  20: 27.5e6,
+  21: 13.2e6,
+  22: 14.7e6,
+  23: 60.6e6,
+  24: 12.5e6
+}
+
 if __name__ == "__main__":
   if len(docopt.sys.argv) < 2:  # Print help message if no options are passed
     docopt.docopt(__doc__, ['-h'])
@@ -35,6 +62,7 @@ if __name__ == "__main__":
     seq_len = hdr[key]['len']
     if idx_wrong.size:
       pylab.hist(ra[idx_wrong].correct_pos, range=[0, seq_len], bins=100, normed=True, histtype='step')
+    pylab.plot([centromere_pos[n+1], centromere_pos[n+1]], [0, pylab.getp(pylab.gca(), 'ylim')[1]], 'k:', lw=5)
     pylab.ylabel('{:s}\n({:2.2f}%)\n{:d}'.format(key, 100*float(idx_wrong.size)/num_total, num_total))
     pylab.setp(pylab.gca(), xlim=[0, seq_len], xticks=[], yticks=[])
 
