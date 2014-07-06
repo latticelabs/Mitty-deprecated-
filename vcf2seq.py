cc571c98c0a189117459ae80b5d7cd459d7d42f4
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-07-06 00:37:37 -0400

    Debugged

--- vcf2seq.py
+++ vcf2seq.py
@@ -19,7 +19,6 @@ __version__ = '0.3.0'
 import h5py
 import docopt
 import numpy
-import struct  # For writing pos and dpos data in binary format (need struct.pack)
 import vcf
 import logging
 
@@ -28,177 +27,11 @@ pos_null = numpy.empty((0,), dtype='u4')
 
 
 def handle_variant(variant):
-  """Write the variant to the mutated sequence and fill out the pos array
-
-  Some setup
-  >>> import io, vcf, struct
-  >>> def add_GT(v, gt):
-  ...   md=vcf.model.make_calldata_tuple('GT')(gt)
-  ...   call=vcf.model._Call(v,'sample',md)
-  ...   v.samples = [call]
-  ...   v._sample_indexes = {'sample': 0}
-
-  Test with SNP: ignore zygosity
-  >>> ref_seq = 'ACTGACTG'; \
-  pos = 2; \
-  ref = 'C'; \
-  alt = [vcf.model._Substitution('T')]; \
-  variant = vcf.model._Record('1', pos, '.', ref, alt, 100, None, None, None, None, None); \
-  add_GT(variant, '1/1'); \
-  f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos)
-  2
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  T
-  >>> _ = f_vseq_pos.seek(0); print struct.unpack('I',f_vseq_pos.read(4))
-  (2,)
-
-  Test with SNP: homo, strand 0
-  >>> f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos, 0)
-  2
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  T
-  >>> _ = f_vseq_pos.seek(0); print struct.unpack('I',f_vseq_pos.read(4))
-  (2,)
-
-
-  Test with SNP: homo, strand 1
-  >>> f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos, 1)
-  2
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  T
-  >>> _ = f_vseq_pos.seek(0); print struct.unpack('I',f_vseq_pos.read(4))
-  (2,)
-
-
-
-  Test with SNP: het, strand 0
-  >>> add_GT(variant, '0/1'); \
-  f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos, 0)
-  1
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  <BLANKLINE>
-  >>> _ = f_vseq_pos.seek(0,2); print f_vseq.tell()
-  0
-
-  Test with SNP: het, strand 1
-  >>> f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos, 1)
-  2
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  T
-  >>> _ = f_vseq_pos.seek(0); print struct.unpack('I',f_vseq_pos.read(4))
-  (2,)
-
-
-  Test with delete: ignore zygosity
-  >>> ref_seq = 'ACTGACTG'; \
-  pos = 2; \
-  ref = 'CTG'; \
-  alt = [vcf.model._Substitution('C')]; \
-  variant = vcf.model._Record('1', pos, '.', ref, alt, 100, None, None, None, None, None); \
-  add_GT(variant, '1/0'); \
-  f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos)
-  4
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  C
-  >>> _ = f_vseq_pos.seek(0); print struct.unpack('I',f_vseq_pos.read(4))
-  (2,)
-
-  Test with same delete, strand 1 (same as REF)
-  >>> f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos, 1)
-  1
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  <BLANKLINE>
-  >>> _ = f_vseq_pos.seek(0,2); print f_vseq.tell()
-  0
-
-
-  Test with delete and .
-  >>> ref_seq = 'ACTGACTG'; \
-  pos = 8; \
-  ref = 'G'; \
-  alt = [vcf.model._Substitution('.')]; \
-  variant = vcf.model._Record('1', pos, '.', ref, alt, 100, None, None, None, None, None); \
-  f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos)
-  8
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  <BLANKLINE>
-  >>> _ = f_vseq_pos.seek(0,2); print f_vseq.tell()
-  0
-
-
-  Test with delete and .
-  >>> ref_seq = 'ACTGACTG'; \
-  pos = 4; \
-  ref = 'GACTG'; \
-  alt = [vcf.model._Substitution('.')]; \
-  variant = vcf.model._Record('1', pos, '.', ref, alt, 100, None, None, None, None, None); \
-  f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos)
-  8
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  <BLANKLINE>
-  >>> _ = f_vseq_pos.seek(0,2); print f_vseq.tell()
-  0
-
-
-  Test with insert
-  >>> ref_seq = 'ACTGACTG'; \
-  pos = 2; \
-  ref = 'C'; \
-  alt = [vcf.model._Substitution('CGGG')]; \
-  variant = vcf.model._Record('1', pos, '.', ref, alt, 100, None, None, None, None, None); \
-  f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos)
-  2
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  CGGG
-  >>> _ = f_vseq_pos.seek(0); print struct.unpack('4I',f_vseq_pos.read(4*4))
-  (2, 3, 3, 3)
-
-
-  Test with insert
-  >>> ref_seq = 'ACTGACTG'; \
-  pos = 8; \
-  ref = 'G'; \
-  alt = [vcf.model._Substitution('GTTT')]; \
-  variant = vcf.model._Record('1', pos, '.', ref, alt, 100, None, None, None, None, None); \
-  f_vseq = io.BytesIO(); \
-  f_vseq_pos = io.BytesIO(); \
-  print handle_variant(variant, f_vseq, f_vseq_pos)
-  8
-  >>> _ = f_vseq.seek(0); print f_vseq.read()
-  GTTT
-  >>> _ = f_vseq_pos.seek(0); print struct.unpack('4I',f_vseq_pos.read(4*4))
-  (8, 9, 9, 9)
-
-  See Readme.md for details of algorithm
-  """
-  #alt = variant.ALT[0].sequence
-  #if alt == '.': alt = ''
-  #ref = variant.REF
-  #if ref == '.': ref = ''
+  """Write the variant to the mutated sequence and fill out the pos array"""
   alt = variant.ALT[0].sequence if variant.ALT[0] is not None else ''
   ref = variant.REF or ''
-  ptr_adv = len(ref) + 1  # You need to be one beyond ref
-  pos_alt = numpy.arange(variant.POS, variant.POS + len(ref), dtype='u4')  # We might have ref bases in alt
+  ptr_adv = len(ref)  # When we get to this function, our pointer is sitting at POS
+  pos_alt = numpy.arange(variant.POS, variant.POS + min(len(alt), len(ref)), dtype='u4')  # We might have ref bases in alt
   if len(alt) > len(ref):  # This was an insertion
     pos_alt = numpy.concatenate((pos_alt, numpy.ones(len(alt) - len(ref), dtype='u4') * (variant.POS + len(ref))))
   if variant.heterozygosity > 0:  # This is heterozygous
@@ -219,7 +52,7 @@ def assemble_sequences(ref_seq, reader):
       # Copy up to this variant
       copy[n] += ref_seq[pointer[n]:variant.POS - 1]
       pos[n] = numpy.concatenate((pos[n], numpy.arange(pointer[n] + 1, variant.POS, dtype='u4')))
-      pointer[n] = variant.POS - 1
+      pointer[n] += max(0, variant.POS - 1 - pointer[n])
 
     # Handle variants
     var_seq, var_pos, ptr_adv = handle_variant(variant)
@@ -231,7 +64,7 @@ def assemble_sequences(ref_seq, reader):
   for n in [0, 1]:
     # Now copy over any residual
     copy[n] += ref_seq[pointer[n]:]
-    pos[n] = numpy.concatenate((pos[n], numpy.arange(pointer[n] + 1, len(ref_seq) + 1, dtype='u4')))
+    pos[n] = numpy.concatenate((pos[n], numpy.arange(pointer[n] + 1, len(ref_seq) + 2, dtype='u4')))
     # Don't forget to add the 'tail': simply an additional, imaginary, base for housekeeping
 
   return copy, pos
