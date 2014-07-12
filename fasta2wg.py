3ae04083a2ffeb3cb020f6b2e362cb48a45dde69
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-07-11 20:07:59 -0400

    Adding inspect function
    Lets us see summary of contents of whole genome .h5 file

--- fasta2wg.py
+++ fasta2wg.py
@@ -3,13 +3,15 @@ list of files is given in the index file (.json)
 
 Usage:
 fasta2wg  --index=IDX  --wg=WG  [--fa=FA] [-v]
+fasta2wg  describe  --wg=WG
 fasta2wg  explain
 
 Options:
   --index=IDX       Index file (.json) listing fasta files to be inserted into whole genome
-  --wg=WG           Name of .wg.gz file to save to (Saved as gzipped .wg file)
+  --wg=WG           Name of whole genome file
   --fa=FA           If set, will also dump a fa.gz file (by simply concatenating the files together) for use by BWA
   -v                Be verbose when you do things
+  describe          Take the indicated whole genome file as input and print a summary of what it contains
   explain           Print index file format and exit
 
 Seven Bridges Genomics
@@ -73,6 +75,20 @@ def concatenate_fasta(file_list, fasta_out):
   _ = subprocess.call('cat {:s} > {:s}'.format(f_list, fasta_out), shell=True)  # We live dangerously
   os.remove(fasta_out + '.sp')  # Clean up after ourselves
 
+
+def describe(h5_fp):
+  print 'Species: {:s}'.format(h5_fp.attrs['species'])
+  print 'Chromosomes: {:d}'.format(len(h5_fp['sequence']))
+  for chrom, v in h5_fp['sequence'].iteritems():
+    for cpy, seq in v.iteritems():
+      try:
+        _ = h5_fp['pos/{:s}/{:s}'.format(chrom, cpy)]
+        mutated = '(mutated)'
+      except KeyError:
+        mutated = ''
+      print 'Chrom {:s}, copy {:s} {:d}bp {:s} ({:s})'.format(chrom, cpy, seq.size, mutated, seq.attrs['seq id'])
+
+
 if __name__ == "__main__":
   if len(docopt.sys.argv) < 2:  # Print help message if no options are passed
     docopt.docopt(__doc__, ['-h'])
@@ -86,10 +102,12 @@ if __name__ == "__main__":
   level = logging.DEBUG if args['-v'] else logging.WARNING
   logging.basicConfig(level=level)
 
-  idx = json.load(open(args['--index'],'r'))
-  f = h5py.File(args['--wg'], "w")
-  save_genome_to_hdf5(idx, f)
-  f.close()
-
-  if args['--fa']:
-    concatenate_fasta([fname for fname_groups in idx['chromosomes'] for fname in fname_groups], args['--fa'])
+  if args['describe']:
+    with h5py.File(args['--wg'], 'r') as fp:
+      describe(fp)
+  else:
+    with h5py.File(args['--wg'], 'w') as fp:
+      idx = json.load(open(args['--index'],'r'))
+      save_genome_to_hdf5(idx, fp)
+    if args['--fa']:
+      concatenate_fasta([fname for fname_groups in idx['chromosomes'] for fname in fname_groups], args['--fa'])
