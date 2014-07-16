384e13fea37183860db0ab040d54e94f4cd70c60
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-07-16 14:54:45 -0400

    Needs work

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='mitty',
     version='1.0.0',
-    packages=find_packages(exclude=['SbgWrappers', 'test-data']),
-    install_requires=['docopt, pysam, pyvcf, numpy']
+    packages=find_packages(exclude=['SbgWrappers', 'README-DATA', 'BigData']),
+    install_requires=['docopt', 'pysam', 'pyvcf', 'numpy', 'matplotlib']
 )
