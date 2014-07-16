ecaf23b458b58f7058d67ae1266776c008b3349d
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-07-16 13:53:33 -0400

    Updating examples for mutations

---
+++ Examples/Mutations/simple_insert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+: File to generate a set of simple insertions
+
+set -x
+DIR=Examples/Mutations
+DATADIR=${DIR}/Out
+
+: Create whole genome file from fasta files
+python fasta2wg.py --index=${DIR}/wg_chimera.json --wg=${DATADIR}/chimera.h5 --fa=${DATADIR}/chimera.fa.gz -v
+: Use the describe command to reveal the contents of the genome file
+python fasta2wg.py describe --wg=${DATADIR}/chimera.h5
+
+: Generate mutations
+python mutate.py  --paramfile ${DIR}/simple_insert.json -v
+: Sort, compress and index the VCF file
+vcf-sort ${DATADIR}/chimera_simple_insert_var.vcf > ${DATADIR}/chimera_simple_insert_var_srt.vcf
+bgzip -c ${DATADIR}/chimera_simple_insert_var_srt.vcf > ${DATADIR}/chimera_simple_insert_var_srt.vcf.gz
+tabix ${DATADIR}/chimera_simple_insert_var_srt.vcf.gz
+
+: Generate the mutated genome
+python vcf2seq.py --ref ${DATADIR}/chimera.h5 --vcf ${DATADIR}/chimera_simple_insert_var_srt.vcf.gz --var ${DATADIR}/chimera_simple_insert.h5 -v
+
+: Use the describe command to reveal the contents of the genome file
+python fasta2wg.py describe --wg=${DATADIR}/chimera_simple_insert.h5
