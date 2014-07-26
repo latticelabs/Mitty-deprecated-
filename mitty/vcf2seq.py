2f9155b57fb0aaf403a6d7b82c0d562ed48e086d
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-07-26 00:33:10 -0400

    New version of vcf2seq - drops mutation locii in whole genome file. Passing tests.

--- mitty/vcf2seq.py
+++ mitty/vcf2seq.py
@@ -23,24 +23,28 @@ import vcf
 import logging
 
 logger = logging.getLogger(__name__)
-pos_null = numpy.empty((0,), dtype='u4')
+pos_null = numpy.empty((0,), dtype='u4')  # Convenient, used in handle_variant
+
+var_type = {'snp': 1, 'indel': 2, 'sv': 3}  # Needed to code the variant type
 
 
 def handle_variant(variant):
   """Write the variant to the mutated sequence and fill out the pos array"""
   alt = variant.ALT[0].sequence if variant.ALT[0] is not None else ''
   ref = variant.REF or ''
+  # The vcf module converts a '.' into a Python None
+
   ptr_adv = len(ref)  # When we get to this function, our pointer is sitting at POS
   pos_alt = numpy.arange(variant.POS, variant.POS + min(len(alt), len(ref)), dtype='u4')  # We might have ref bases in alt
   if len(alt) > len(ref):  # This was an insertion
     pos_alt = numpy.concatenate((pos_alt, numpy.ones(len(alt) - len(ref), dtype='u4') * (variant.POS + len(ref))))
   if variant.heterozygosity > 0:  # This is heterozygous
     if variant.samples[0].gt_nums[0] == '1':
-      return [alt, ''], [pos_alt, pos_null], [ptr_adv, 0]
+      return [alt, ''], [pos_alt, pos_null], [ptr_adv, 0], 1  # Het copy 1
     else:
-      return ['', alt], [pos_null, pos_alt], [0, ptr_adv]
+      return ['', alt], [pos_null, pos_alt], [0, ptr_adv], 2  # Het copy 2
   else:
-    return [alt, alt], [pos_alt, pos_alt], [ptr_adv, ptr_adv]
+    return [alt, alt], [pos_alt, pos_alt], [ptr_adv, ptr_adv], 0  # Homozygous
 
 
 def assemble_sequences(ref_seq, reader):
@@ -48,10 +52,9 @@ def assemble_sequences(ref_seq, reader):
   pos = [[pos_null], [pos_null]]
   pointer = [0, 0]  # Which part of the reference are we reading, for each copy of the chromosome
   mutated_pointer = [0, 0]  # Where are we on the mutated sequence (needed for marking variants)
-  # (type, (start, stop))  'type' is the same as variant.var_subtype
-  variant_coordinates = [
-    {'ins': []}, {'ins': []}  # A list of variants and their coordinates in the mutated sequence, one for each copy
-  ]
+  variant_coordinates = []  # n x 2 x 2 (variants, chromosome copies, [start, stop])
+  variant_codes = []  # n x 2  (variants, [code, het])
+
   for variant in reader:
     for n in [0, 1]:
       # Copy up to this variant
@@ -61,14 +64,19 @@ def assemble_sequences(ref_seq, reader):
       mutated_pointer[n] += len(copy[n][-1])
 
     # Handle variants
-    var_seq, var_pos, ptr_adv = handle_variant(variant)
+    var_seq, var_pos, ptr_adv, het = handle_variant(variant)
+    variant_codes.append([var_type[variant.var_type], het])
+    this_vc = []
     for n in [0, 1]:
       copy[n].append(var_seq[n])
       pos[n].append(var_pos[n])
       pointer[n] += ptr_adv[n]
-      if len(copy[n][-1]) and variant.var_subtype == 'ins':  # Only doing insertions for now
-        variant_coordinates[n]['ins'].append([mutated_pointer[n], mutated_pointer[n] + len(copy[n][-1])])
+
+      this_vc.append([mutated_pointer[n], mutated_pointer[n] + len(copy[n][-1])])
+
       mutated_pointer[n] += len(copy[n][-1])
+
+    variant_coordinates.append(this_vc)
 
     logger.debug('({:d}%, {:d}%)'.format(100*pointer[0]/len(ref_seq), 100*pointer[1]/len(ref_seq)))
 
@@ -78,7 +86,7 @@ def assemble_sequences(ref_seq, reader):
     pos[n].append(numpy.arange(pointer[n] + 1, len(ref_seq) + 2, dtype='u4'))
     # Don't forget to add the 'tail': simply an additional, imaginary, base for housekeeping
 
-  return (''.join(copy[0]), ''.join(copy[1])), (numpy.concatenate(pos[0]), numpy.concatenate(pos[1])), variant_coordinates
+  return (''.join(copy[0]), ''.join(copy[1])), (numpy.concatenate(pos[0]), numpy.concatenate(pos[1])), variant_coordinates, variant_codes
 
 
 def main(args):
@@ -87,27 +95,28 @@ def main(args):
     logger.debug('Writing to {:s}'.format(var_fp.filename))
     var_fp.attrs['species'] = ref_fp.attrs['species']
     var_fp.create_group('sequence')
-    for chrom in [int(c) for c in ref_fp['sequence']]:  #h5 keys are unicode
+    for chrom in [int(c) for c in ref_fp['sequence']]:  # h5 keys are unicode
       logger.debug('Assembling chromosome {:d}'.format(chrom))
       # We assume the reference is haploid
       ref_seq = ref_fp['sequence/{:d}/1'.format(chrom)][:].tostring()  # Very cheap operation
       chrom_grp = var_fp['sequence'].create_group(str(chrom))
       chrom_grp.attrs['seq_id'] = ref_fp['sequence/{:d}'.format(chrom)].attrs['seq_id']
       try:
-        copy, pos, var_coords = assemble_sequences(ref_seq, vcf_reader.fetch(chrom=chrom, start=0, end=len(ref_seq)))
+        copy, pos, var_coords, var_codes = assemble_sequences(ref_seq, vcf_reader.fetch(chrom=chrom, start=0, end=len(ref_seq)))
       except KeyError:  # No mutations for this chromosome
         logger.debug('Chromosome {:d} is unaltered, copying reference'.format(chrom))
         copy = [ref_seq, ref_seq]  # Same as reference
         pos = [None, None]
-        var_coords = [None, None]
-      for n, (this_copy, this_pos, this_vc) in enumerate(zip(copy, pos, var_coords)):
+        var_coords, var_codes = [None, None]
+      for n, (this_copy, this_pos) in enumerate(zip(copy, pos)):
         dset = chrom_grp.create_dataset('{:d}'.format(n+1), data=numpy.fromstring(this_copy, dtype='u1'))
         dset.attrs['reference'] = True
         if this_pos is not None:  # We have variants
           dset.attrs['reference'] = False
           var_fp.create_dataset('pos/{:d}/{:d}'.format(chrom, n+1), data=this_pos)
-          for k, v in this_vc.iteritems():
-            var_fp.create_dataset('variant_pos/{:s}/{:d}/{:d}'.format(k, chrom, n+1), data=numpy.array(v, dtype='u4'))
+      if var_coords:
+        var_fp.create_dataset('variants/pos/{:d}'.format(chrom), data=numpy.array(var_coords, dtype='u4'))
+        var_fp.create_dataset('variants/codes/{:d}'.format(chrom), data=numpy.array(var_codes, dtype='u1'))
 
 
 if __name__ == "__main__":
--- tests/vcf2seq_test.py
+++ tests/vcf2seq_test.py
@@ -1,14 +1,17 @@
 import vcf
 import io
+import numpy.testing
 from mitty import vcf2seq
 from nose.tools import assert_equals
 
 
-def assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, variant_coords_1, var_seq_2, pos_2, variant_coords_2):
+def assembly_check(ref_seq, vcf_str,
+                   var_seq_1, pos_1, var_seq_2, pos_2,
+                   v_coords, v_codes):
   """Utility function to test if two sequence assemblies and their pos vectors are the same."""
   def check(tup1, tup2):
-    copy_1, p_1, vc_dict_1 = tup1
-    copy_2, p_2, vc_dict_2 = tup2
+    copy_1, p_1, v_pos_1, v_cod_1 = tup1
+    copy_2, p_2, v_pos_2, v_cod_2 = tup2
     for n in [0,1]:
       assert_equals(len(copy_1[n]), len(copy_2[n]))
       assert_equals(len(p_1[n]), len(p_2[n]))
@@ -16,12 +19,12 @@ def assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, variant_coords_1, var_seq
       for x, y in zip(copy_1[n], copy_2[n]):
         assert_equals(x, y)
 
-      for x, y in zip(p_1[n], p_2[n]):
-        assert_equals(x, y)
+      numpy.testing.assert_array_equal(p_1[n], p_2[n])
 
-      assert_equals(vc_dict_1[n], vc_dict_2[n])
+    numpy.testing.assert_array_equal(v_pos_1, v_pos_2, err_msg=str(v_pos_1) + '|' + str(v_pos_2))
+    numpy.testing.assert_array_equal(v_cod_1, v_cod_2)
 
-  a2 = ([var_seq_1, var_seq_2], [pos_1, pos_2], [variant_coords_1, variant_coords_2])
+  a2 = ([var_seq_1, var_seq_2], [pos_1, pos_2], v_coords, v_codes)
   a1 = vcf2seq.assemble_sequences(ref_seq, vcf.Reader(fsock=io.BytesIO(vcf_str)))
   check(a1, a2)
 
@@ -35,12 +38,12 @@ def test_assemble_sequences_homo_snp():
   )
   var_seq_1 = 'ATTG'
   pos_1 = [1, 2, 3, 4, 5]
-  vc_1 = {'ins': []}
   var_seq_2 = 'ATTG'
   pos_2 = [1, 2, 3, 4, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 2], [1, 2]]]
+  v_codes = [[1, 0]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_snp():
@@ -52,12 +55,12 @@ def test_assemble_sequences_hetero_snp():
   )
   var_seq_1 = 'ATTG'
   pos_1 = [1, 2, 3, 4, 5]
-  vc_1 = {'ins': []}
   var_seq_2 = 'ACTG'
   pos_2 = [1, 2, 3, 4, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 2], [1, 1]]]
+  v_codes = [[1, 1]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_ins():
@@ -69,12 +72,12 @@ def test_assemble_sequences_hetero_ins():
   )
   var_seq_1 = 'ACCTG'
   pos_1 = [1, 2, 3, 3, 4, 5]
-  vc_1 = {'ins': [[1, 3]]}
   var_seq_2 = 'ACTG'
   pos_2 = [1, 2, 3, 4, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 3], [1, 1]]]
+  v_codes = [[2, 1]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_del():
@@ -86,12 +89,12 @@ def test_assemble_sequences_hetero_del():
   )
   var_seq_1 = 'ACTG'
   pos_1 = [1, 2, 3, 4, 5]
-  vc_1 = {'ins': []}
   var_seq_2 = 'ATG'
   pos_2 = [1, 3, 4, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 1], [1, 1]]]
+  v_codes = [[2, 2]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_del2():
@@ -103,12 +106,12 @@ def test_assemble_sequences_hetero_del2():
   )
   var_seq_1 = 'ACTG'
   pos_1 = [1, 2, 3, 4, 5]
-  vc_1 = {'ins': []}
   var_seq_2 = 'ACG'
   pos_2 = [1, 2, 4, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 1], [1, 2]]]
+  v_codes = [[2, 2]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_same_locus_del():
@@ -121,12 +124,12 @@ def test_assemble_sequences_hetero_same_locus_del():
   )
   var_seq_1 = 'ATG'
   pos_1 = [1, 3, 4, 5]
-  vc_1 = {'ins': []}
   var_seq_2 = 'ATG'
   pos_2 = [1, 3, 4, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 1], [1, 1]], [[1, 1], [1, 1]]]
+  v_codes = [[2, 2], [2, 1]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_same_locus_ins_del():
@@ -139,12 +142,12 @@ def test_assemble_sequences_hetero_same_locus_ins_del():
   )
   var_seq_1 = 'ACTTG'
   pos_1 = [1, 2, 3, 3, 4, 5]
-  vc_1 = {'ins': [[1,3]]}
   var_seq_2 = 'ATG'
   pos_2 = [1, 3, 4, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 1], [1, 1]], [[1, 3], [1, 1]]]
+  v_codes = [[2, 2], [2, 1]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_same_locus_ins_del2():
@@ -157,12 +160,12 @@ def test_assemble_sequences_hetero_same_locus_ins_del2():
   )
   var_seq_1 = 'ACTTG'
   pos_1 = [1, 2, 3, 3, 4, 5]
-  vc_1 = {'ins': [[1,3]]}
   var_seq_2 = 'A'
   pos_2 = [1, 5]
-  vc_2 = {'ins': []}
+  v_coords = [[[1, 1], [1, 1]], [[1, 3], [1, 1]]]
+  v_codes = [[2, 2], [2, 1]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_same_locus_inv_del():
@@ -175,12 +178,13 @@ def test_assemble_sequences_hetero_same_locus_inv_del():
   )
   var_seq_1 = 'ATG'
   pos_1 = [1, 3, 4, 5]
-  vc_1 = {'ins': []}
   var_seq_2 = 'AGAG'
   pos_2 = [1, 2, 3, 4, 5]
-  vc_2 = {'ins': [[1, 3]]}  # This is interesting, because vcf flags this as an insertion
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
+  v_coords = [[[1, 1], [1, 3]], [[1, 1], [3, 3]]]
+  v_codes = [[2, 2], [2, 1]]
+
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
 
 def test_assemble_sequences_hetero_same_locus_longer_ins():
@@ -195,39 +199,10 @@ def test_assemble_sequences_hetero_same_locus_longer_ins():
   )
   var_seq_1 = 'ACGGTGTT'
   pos_1 = [1, 2, 3, 3, 3, 4, 5, 5, 5]
-  vc_1 = {'ins': [[1,4], [5, 8]]}
   var_seq_2 = 'ACAATGAA'
   pos_2 = [1, 2, 3, 3, 3, 4, 5, 5, 5]
-  vc_2 = {'ins': [[1, 4], [5, 8]]}
+  v_coords = [[[1, 1], [1, 4]], [[1, 4], [4, 4]], [[5, 5], [5, 8]], [[5, 8], [8 ,8]]]
+  v_codes = [[2, 2], [2, 1], [2, 2], [2, 1]]
 
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
-
-
-def test_assemble_sequences_misc():
-  """Testing a more comprehensive VCF file"""
-  ref_seq = 'ACTGACTGACTG'
-  vcf_str = (
-    "##fileformat=VCFv4.1\n"
-    "##fileDate={:s}\n"
-    "##source=mutate.py {:s} ({:s})\n"
-    "##reference={:s}\n"
-    "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tsample\n"
-    "1\t1\t.\tA\tT\t100\tPASS\t.\tGT\t0/1\n"
-    "1\t2\t.\tC\tT\t100\tPASS\t.\tGT\t1/0\n"
-    "1\t3\t.\tT\tG\t100\tPASS\t.\tGT\t1/1\n"
-    "1\t4\t.\tG\t.\t100\tPASS\t.\tGT\t0/1\n"
-    "1\t5\t.\tAC\tA\t100\tPASS\t.\tGT\t1/0\n"
-    "1\t6\t.\tCT\tC\t100\tPASS\t.\tGT\t0/1\n"
-    "1\t8\t.\tG\tGC\t100\tPASS\t.\tGT\t0/1\n"
-    "1\t8\t.\tG\tGG\t100\tPASS\t.\tGT\t1/0\n"
-  )
-  var_seq_1 = 'ATGGATGGACTG'
-  pos_1 = [1,2,3,4,5,7,8,9,9,10,11,12, 13]
-  vc_1 = {'ins': [[6, 8]]}
-  var_seq_2 = 'TCGACGCACTG'
-  pos_2 = [1,2,3,5,6,8,9,9,10,11,12, 13]
-  vc_2 = {'ins': [[5, 7]]}
-
-  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, vc_1, var_seq_2, pos_2, vc_2)
-
+  assembly_check(ref_seq, vcf_str, var_seq_1, pos_1, var_seq_2, pos_2, v_coords, v_codes)
 
