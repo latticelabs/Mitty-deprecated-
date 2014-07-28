c77711873b023f9e4a6a9b9af912ce1dd28fa890
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-07-28 15:29:26 -0400

    Now can run tests from any directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('../mitty')  # This lets tests be run from pretty much anywhere
-
 import tempfile
 import os
 import h5py
@@ -8,6 +5,7 @@ from mitty.fasta2wg import save_genome_to_hdf5
 from shutil import rmtree
 
 # These need to be available to the rest of the test suite for the mutations module
+source_tree_root = os.path.join(os.path.dirname(__file__), os.path.pardir)
 data_dir = tempfile.gettempdir()
 wg_name = os.path.join(data_dir, 'test.h5')
 
@@ -18,15 +16,14 @@ def setup_package():
   if not os.path.exists(data_dir):
     os.makedirs(data_dir)
 
-  # This setup only works when called from the project root directory such that Data is accessible
   index = {
     "header": {
         "species": "Test Chimera"
     },
     "chromosomes": [
-        ["Data/adenovirus.fa.gz"],
-        ["Data/porcine_circovirus.fa.gz", "Data/porcine_circovirus.fa.gz"],
-        ["Data/altered_porcine.fa.gz"]
+        [source_tree_root + "/Data/adenovirus.fa.gz"],
+        [source_tree_root + "/Data/porcine_circovirus.fa.gz", source_tree_root + "/Data/porcine_circovirus.fa.gz"],
+        [source_tree_root + "/Data/altered_porcine.fa.gz".format(source_tree_root)]
     ]
   }
   with h5py.File(wg_name, 'w') as h5_fp:
