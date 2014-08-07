3f45e5c55773ba2fa1e8c4e271d3c209a9d5a07d
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-08-07 12:04:14 -0400

    Adding bitarray to requirements

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='mitty',
     version='1.0.0',
     packages=find_packages(exclude=['SbgWrappers', 'README-DATA', 'BigData']),
-    install_requires=['docopt', 'pysam', 'pyvcf', 'numpy', 'matplotlib', 'h5py', 'nose', 'scipy']
+    install_requires=['docopt', 'pysam', 'pyvcf', 'numpy', 'matplotlib', 'h5py', 'nose', 'scipy', 'bitarray']
 )
