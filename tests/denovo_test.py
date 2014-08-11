d4b00bf559203f6af294fcf15e79499337214474
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-08-11 13:10:15 -0400

    Now covers all important parts of denovo

--- tests/denovo_test.py
+++ tests/denovo_test.py
@@ -66,4 +66,37 @@ def load_variant_models_test():
   assert 'master_seed' in mdl[1]["params"]
 
 
-# This test uses the stock SNP which must be functional for this test to pass
+# This test uses the stock SNP which must be functional for this test to pass as well as some files created during
+# test setup
+def main_test():
+  import json, vcf
+  param_json = {
+    "variant_models": [
+      {
+        "snp": {
+           "chromosome": [1, 2],
+           "phet": 0.5,
+           "p": 0.0001,
+           "master_seed": 1
+        }
+      },
+      {
+        "snp": {
+           "chromosome": [2],
+           "phet": 0.0,
+           "p": 0.01,
+           "master_seed": 2
+        }
+      }
+    ]
+  }
+  _, param_fname = tempfile.mkstemp(suffix='.json')
+  with open(param_fname, 'w') as fp:
+    json.dump(param_json, fp, indent=2)
+  _, vcf_file_fname = tempfile.mkstemp(suffix='.vcf.gz')
+  g1 = main(wg_name, vcf_file_name=vcf_file_fname, param_file_name=param_fname, master_seed=1)
+  assert os.path.exists(vcf_file_fname)
+
+  vcf_rdr = vcf.Reader(filename=vcf_file_fname).fetch(chrom=1, start=0)
+  v1 = vcf_rdr.next()
+  assert v1.POS, g1[1][0].POS
