dadc954fdc185e1750da9949fdd017772c80f78f
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-02 18:09:12 -0400

    Useful utility functions for dealing with plugins

---
+++ mitty/Plugins/putil.py
@@ -0,0 +1,32 @@
+"""Useful utility functions for dealing with plugins."""
+import pkgutil
+import importlib
+import os
+variant_plugin_dir = os.path.join(os.path.dirname(__file__), 'variants')
+read_plugin_dir = os.path.join(os.path.dirname(__file__), 'Reads')
+
+
+def load_variant_plugin(plugin_name):
+  return importlib.import_module('mitty.Plugins.variants.' + plugin_name + '_plugin')
+
+
+def list_all_variant_plugins():
+  return [package_name[:-7]
+          for _, package_name, _ in pkgutil.iter_modules([variant_plugin_dir]) if package_name.endswith('_plugin')]
+
+
+def load_all_variant_plugins():
+  return [(name, load_variant_plugin(name)) for name in list_all_variant_plugins()]
+
+
+def load_read_plugin(plugin_name):
+  return importlib.import_module('mitty.Plugins.Reads.' + plugin_name + '_plugin')
+
+
+def list_all_read_plugins():
+  return [package_name[:-7]
+          for _, package_name, _ in pkgutil.iter_modules([read_plugin_dir]) if package_name.endswith('_plugin')]
+
+
+def load_all_read_plugins():
+  return [(name, load_read_plugin(name)) for name in list_all_read_plugins()]
