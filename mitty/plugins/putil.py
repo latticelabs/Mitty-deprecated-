9f4dbf6cf8bf3b6b9b9531d9fe4f84fef0148989
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-03 13:39:50 -0400

    Changing case of plugins directory

--- mitty/plugins/putil.py
+++ mitty/plugins/putil.py
@@ -7,7 +7,7 @@ read_plugin_dir = os.path.join(os.path.dirname(__file__), 'reads')
 
 
 def load_variant_plugin(plugin_name):
-  return importlib.import_module('mitty.Plugins.variants.' + plugin_name + '_plugin')
+  return importlib.import_module('mitty.plugins.variants.' + plugin_name + '_plugin')
 
 
 def list_all_variant_plugins():
@@ -20,7 +20,7 @@ def load_all_variant_plugins():
 
 
 def load_read_plugin(plugin_name):
-  return importlib.import_module('mitty.Plugins.reads.' + plugin_name + '_plugin')
+  return importlib.import_module('mitty.plugins.reads.' + plugin_name + '_plugin')
 
 
 def list_all_read_plugins():
