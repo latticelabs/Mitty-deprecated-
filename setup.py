6fc4062f52e5c3d95fd55b9785d7e196dee70add
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-11 00:35:06 -0400

    Forgot to add subdirs of plugins

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Simulator for genomic data',
     author='Seven Bridges Genomics',
     author_email='kaushik.ghose@sbgenomics.com',
-    packages=['mitty', 'mitty.lib', 'mitty.plugins', 'mitty.util'],
+    packages=['mitty', 'mitty.lib', 'mitty.plugins', 'mitty.plugins.reads', 'mitty.plugins.variants', 'mitty.util'],
     scripts=['mitty/denovo.py', 'mitty/checkbam.py', 'mitty/population.py', 'mitty/reads2bam.py', 'mitty/vcf2reads.py'],
     install_requires=[
       'docopt >= 0.6.1',
