88a2f5ca6a5a9cd79c21d1697917252151e4b8db
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-11 13:28:02 -0400

    Updating setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mitty',
@@ -6,15 +6,31 @@ setup(
     description='Simulator for genomic data',
     author='Seven Bridges Genomics',
     author_email='kaushik.ghose@sbgenomics.com',
-    packages=['mitty', 'mitty.lib', 'mitty.plugins', 'mitty.plugins.reads', 'mitty.plugins.variants', 'mitty.util'],
+    #packages=['mitty', 'mitty.lib', 'mitty.plugins', 'mitty.plugins.reads', 'mitty.plugins.variants', 'mitty.util'],
+#    packages=['mitty', 'mitty.lib', 'mitty.plugins', 'mitty.plugins.reads', 'mitty.plugins.variants',
+#              'tests', 'tests.'],
+    packages=find_packages(include=['mitty', 'tests']),
     scripts=['mitty/denovo.py', 'mitty/checkbam.py', 'mitty/population.py', 'mitty/reads2bam.py', 'mitty/vcf2reads.py'],
     install_requires=[
-      'docopt >= 0.6.1',
-      'pysam >= 0.8.0',
-      'pyvcf >= 0.6.7',
-      'numpy >= 1.9.0',
-      'matplotlib >= 1.4.0',
-      'nose >= 1.3.4',
-      'scipy >= 0.14.0'
-    ]
+      'distribute>=0.7.3',
+      'setuptools>=0.7',
+      'Cython==0.21',
+      'PyVCF>=0.6.7',
+      'docopt==0.6.2',
+      'matplotlib==1.4.0',
+      'mock==1.0.1',
+      'nose==1.3.4',
+      'numpy==1.9.0',
+      'pyparsing==2.0.2',
+      'pysam==0.8.0',
+      'python-dateutil==2.2',
+      'six==1.7.3',
+      'wsgiref==0.1.2'
+    ],
+    data_files=[('examples/data', ['examples/data/chr1.fa',
+                                   'examples/data/chr2.fa',
+                                   'examples/data/chr3.fa',
+                                   'examples/data/chr4.fa',
+                                   'examples/data/chimera.fa',
+                                   'examples/data/chimera.fa.gz'])]
 )
