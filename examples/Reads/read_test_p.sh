08909d427502bd9b348a7ce04085b7fbf0c97091
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-14 23:21:51 -0400

    Cleaning up examples

--- examples/Reads/read_test_p.sh
+++ examples/Reads/read_test_p.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -xe
 
-pushd `dirname $0`
+#pushd `dirname $0`
 
 : This shell script tests generating reads
 PROGDIR=../../mitty/
 GENOMEDIR=../data/
-OUTDIR="Out/"
+OUTDIR=Out/
 PFILE=seq_reads_p.json
 
 mkdir -p ${OUTDIR}
 
 : Create null reads from test genome
-python ${PROGDIR}vcf2reads.py --fa_dir=${GENOMEDIR}  --out=${OUTDIR}null  --pfile=${PFILE}  --block_len=1000 --master_seed=1 -V
+python ${PROGDIR}vcf2reads.py --pfile=${PFILE}  -V
 
 : Do the fake alignment to check
-python ${PROGDIR}reads2bam.py -p --fa_dir=${GENOMEDIR} --fastq ${OUTDIR}null.fq --bam=${OUTDIR}test.bam -v
+python ${PROGDIR}reads2bam.py -p --fa_dir=${GENOMEDIR} --fastq ${OUTDIR}reads_p.fq --bam=${OUTDIR}reads_p_aligned.bam -v
