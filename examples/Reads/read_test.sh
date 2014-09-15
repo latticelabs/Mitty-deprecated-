c35b0a528150a0be74346e8f8e870236e6542ee2
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-15 11:00:27 -0400

    Adding comprehensive example for taking reads

--- examples/Reads/read_test.sh
+++ examples/Reads/read_test.sh
@@ -8,7 +8,17 @@ PROGDIR=../../mitty/
 GENOMEDIR=../data
 OUTDIR=Out/
 
-if [ $1 == "nullseq" ]
+if [ $# -ne 1 ]
+then
+  echo "Arguments:"
+  echo "variants, nullseq, nullillumina, varillumina"
+  exit 0
+elif [ $1 == "variants" ]
+then
+  python ${PROGDIR}denovo.py --pfile=variants.json  -v
+  cat variants.vcf
+  exit 0
+elif [ $1 == "nullseq" ]
 then
   PFILE=seq_reads.json
 elif [ $1 == "nullillumina" ]
@@ -16,7 +26,9 @@ then
   PFILE=null_illumina_reads.json
 elif  [ $1 == "varillumina" ]
 then
-  PFILE=var_illumina_reads_p.json
+  PFILE=var_illumina_reads.json
+else
+  exit 0
 fi
 
 mkdir -p ${OUTDIR}
