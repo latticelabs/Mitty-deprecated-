540ecbba326f6add1bc87036b23b93aa3227ce75
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-16 12:02:51 -0400

    Code clean up, added parameter assertions

--- mitty/plugins/variants/delete_plugin.py
+++ mitty/plugins/variants/delete_plugin.py
@@ -5,6 +5,7 @@ Note: This never generates a deletion at the first base of a sequence.
 """
 import numpy
 import logging
+from mitty.lib import SEED_MAX
 from mitty.lib.variation import Variation
 import mitty.plugins.variants.util as util
 
@@ -13,15 +14,11 @@ logger = logging.getLogger(__name__)
 
 __example_param_text__ = """
 {
-  "chromosome": [1],      # List of chromosomes to apply the variant to
-  "p": 0.01,              # probability that the deletion will happen at any given base
-  "phet": 0.5,            # probability that the variant will be heterozygous
-  "del_len_lo": 100,      # lower bound on deletion lengths
-  "del_len_hi": 10000,    # upper bound on deletion lengths
-  "del_loc_rng_seed": 1,  #
-  "del_len_rng_seed": 2,
-  "het_rng_seed": 3,
-  "copy_rng_seed": 4
+  "chromosome": [1],    # List of chromosomes to apply the variant to
+  "p": 0.01,            # probability that the deletion will happen at any given base
+  "phet": 0.5,          # probability that the variant will be heterozygous
+  "del_len_lo": 10,      # lower bound on deletion lengths
+  "del_len_hi": 20,     # upper bound on deletion lengths
 }
 """
 
@@ -40,19 +37,14 @@ def variant_generator(ref={},
                       del_len_hi=100,
                       master_seed=1,
                       **kwargs):
-  try:
-    vg = variant_generator
-
-    del_loc_rng, del_len_rng, het_rng, copy_rng = vg.del_loc_rng, vg.del_len_rng, vg.het_rng, vg.copy_rng
-    logger.debug('Using previous RNG states')
-  except AttributeError:
-    del_loc_rng_seed, del_len_rng_seed, het_rng_seed, copy_rng_seed = \
-      numpy.random.RandomState(seed=master_seed).randint(2<<32-1, size=4)
-    logger.debug('Used master seed to generate seeds {:d}, {:d}, {:d}, {:d}'.
-                 format(del_loc_rng_seed, del_len_rng_seed, het_rng_seed, copy_rng_seed))
-    del_loc_rng, del_len_rng, het_rng, copy_rng = util.initialize_rngs(del_loc_rng_seed, del_len_rng_seed, het_rng_seed, copy_rng_seed)
-    vg = variant_generator
-    vg.del_loc_rng, vg.del_len_rng, vg.het_rng, vg.copy_rng = del_loc_rng, del_len_rng, het_rng, copy_rng
+  assert 0 <= p <= 1.0, "Probability out of range"
+  assert 0 <= phet <= 1.0, "Probability out of range"
+  assert 0 < del_len_lo <= del_len_hi, "Check deletion length values"
+  del_loc_rng_seed, del_len_rng_seed, het_rng_seed, copy_rng_seed = \
+    numpy.random.RandomState(seed=master_seed).randint(SEED_MAX, size=4)
+  logger.debug('Used master seed to generate seeds {:d}, {:d}, {:d}, {:d}'.
+               format(del_loc_rng_seed, del_len_rng_seed, het_rng_seed, copy_rng_seed))
+  del_loc_rng, del_len_rng, het_rng, copy_rng = util.initialize_rngs(del_loc_rng_seed, del_len_rng_seed, het_rng_seed, copy_rng_seed)
 
   for chrom in chromosome:
     ref_seq = ref[chrom]  # Very cheap operation
