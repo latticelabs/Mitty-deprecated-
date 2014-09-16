a0525428ee2fa8b156b6f2b2ca9d242035c3334d
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-09-16 08:48:03 -0400

    Improve docs, remove cruft

--- mitty/plugins/variants/insert_plugin.py
+++ mitty/plugins/variants/insert_plugin.py
@@ -3,29 +3,30 @@
 Note: This never generates a deletion at the first base of a sequence.
 
 """
-__explain__ = """
-Example parameter snippet:
-
-    {
-        "chromosome": [1],
-        "phet": 0.5,
-        "p": 0.01,
-        "ins_len_lo": 100,
-        "ins_len_hi": 10000,
-        "ins_loc_rng_seed": 1,
-        "ins_len_rng_seed": 2,
-        "base_sel_rng_seed": 3,
-        "het_rng_seed": 4,
-        "copy_rng_seed": 5
-    }
+__example_param_text = """
+{
+  "chromosome": [1],
+  "phet": 0.5,
+  "p": 0.001,
+  "ins_len_lo": 5,
+  "ins_len_hi": 10
+}
 """
-import string
+
+_description = """
+This insertion model is designed more for testing algorithms than for generating a natural distribution of insertions.
+If you need a exponential/power law like distribution of insertions see exp_inserts.
+A typical parameter set resembles
+""" + __example_param_text
+
+_example_params = eval(__example_param_text)
+
+
 import numpy
-import logging
 import mitty.plugins.variants.util as util
 from mitty.lib.variation import Variation
-
-
+from mitty.lib import SEED_MAX
+import logging
 logger = logging.getLogger(__name__)
 
 
@@ -35,38 +36,16 @@ def variant_generator(ref={},
                       phet=0.5,
                       ins_len_lo=100,
                       ins_len_hi=10000,
-                      master_seed=None,
-                      ins_loc_rng_seed=1,
-                      ins_len_rng_seed=2,
-                      base_sel_rng_seed=3,
-                      het_rng_seed=4,
-                      copy_rng_seed=5,
+                      master_seed=1,
                       **kwargs):
   
-  try:
-      vg = variant_generator
-      
-      ins_loc_rng, ins_len_rng,base_sel_rng, het_rng, copy_rng = vg.ins_loc_rng, vg.ins_len_rng,vg.base_sel_rng, vg.het_rng, vg.copy_rng
-      logger.debug('Using previous RNG states')
-  
-  except AttributeError:
-      if master_seed is not None:
+  ins_loc_rng_seed, ins_len_rng_seed, base_sel_rng_seed, het_rng_seed, copy_rng_seed = \
+    numpy.random.RandomState(seed=master_seed).randint(SEED_MAX, size=5)
+  logger.debug('Used master seed to generate seeds {:d}, {:d}, {:d}, {:d}, {:d}'.
+               format(ins_loc_rng_seed, ins_len_rng_seed, base_sel_rng_seed, het_rng_seed, copy_rng_seed))
 
-        ins_loc_rng_seed, ins_len_rng_seed, base_sel_rng_seed, het_rng_seed, copy_rng_seed = \
-        numpy.random.RandomState(seed=master_seed).randint(100000000, size=5)
-        logger.debug('Used master seed to generate seeds {:d}, {:d}, {:d}, {:d}, {:d}'.
-                 format(ins_loc_rng_seed, ins_len_rng_seed, base_sel_rng_seed, het_rng_seed, copy_rng_seed))
-
-
-      ins_loc_rng, ins_len_rng,base_sel_rng, het_rng, copy_rng= util.initialize_rngs(ins_loc_rng_seed, ins_len_rng_seed,
-                                                                        base_sel_rng_seed, het_rng_seed, copy_rng_seed)
-      vg = variant_generator
-      vg.ins_loc_rng, vg.ins_len_rng,vg.base_sel_rng, vg.het_rng, vg.copy_rng= \
-      ins_loc_rng, ins_len_rng,base_sel_rng, het_rng, copy_rng
-      
-
-
-
+  ins_loc_rng, ins_len_rng, base_sel_rng, het_rng, copy_rng = \
+    util.initialize_rngs(ins_loc_rng_seed, ins_len_rng_seed, base_sel_rng_seed, het_rng_seed, copy_rng_seed)
 
   for chrom in chromosome:
     ref_seq = ref[chrom]
@@ -74,12 +53,10 @@ def variant_generator(ref={},
     if ins_locs.size == 0: continue  # No variants here
     ins_lens = ins_len_rng.randint(low=ins_len_lo, high=ins_len_hi+1, size=ins_locs.size)
     het_type = util.het(ins_locs.size, phet, het_rng, copy_rng)
-    
 
     yield {chrom:[Variation(pos + 1, pos + 2, ref_seq[pos], ref_seq[pos] +
-            base_sel_rng.choice(['A','C','G','T'], size=ins_len, replace=True, p=[.3, .2, .2, .3]).tostring(), het)
-              for het, pos, ins_len in zip(het_type, ins_locs, ins_lens) if ref_seq[pos] != 'N' and ref_seq[pos] != '\n']}
+           base_sel_rng.choice(['A','C','G','T'], size=ins_len, replace=True, p=[.3, .2, .2, .3]).tostring(), het)
+           for het, pos, ins_len in zip(het_type, ins_locs, ins_lens) if ref_seq[pos] != 'N']}
 
 if __name__ == "__main__":
-    print __explain__
-
+    print _description
