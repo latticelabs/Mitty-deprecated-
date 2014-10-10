26a3f9d54147e79e88a2684ab268332641166d7e
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2014-10-10 13:55:15 -0400

    Added correct version of pyvcf (github) in requirements.txt
    This allows us to install the correct version of pyvcf using setup.

--- setup.py
+++ setup.py
@@ -8,21 +8,22 @@ setup(
     author_email='kaushik.ghose@sbgenomics.com',
     packages=find_packages(include=['mitty*'], exclude=['mitty.util']),
     scripts=['mitty/denovo.py', 'mitty/checkbam.py', 'mitty/population.py', 'mitty/reads2bam.py', 'mitty/vcf2reads.py'],
-    install_requires=[
-      'distribute>=0.7.3',
-      'setuptools>=0.7',
-      'Cython==0.21',
-      'PyVCF>=0.6.7',
-      'docopt==0.6.2',
-      'mock==1.0.1',
-      'nose==1.3.4',
-      'numpy==1.9.0',
-      'pyparsing==2.0.2',
-      'pysam==0.8.0',
-      'python-dateutil==2.2',
-      'six==1.7.3',
-      'wsgiref==0.1.2'
-    ],
+    # install_requires=[
+    #   'distribute>=0.7.3',
+    #   'setuptools>=0.7',
+    #   'Cython==0.21',
+    #   'PyVCF>=0.6.7',
+    #   'docopt==0.6.2',
+    #   'mock==1.0.1',
+    #   'nose==1.3.4',
+    #   'numpy==1.9.0',
+    #   'pyparsing==2.0.2',
+    #   'pysam==0.8.0',
+    #   'python-dateutil==2.2',
+    #   'six==1.7.3',
+    #   'wsgiref==0.1.2'
+    # ],
+    # dependency_links=['git+https://github.com/jamescasbon/PyVCF.git'],
     include_package_data=True,
-    package_data = {'mitty': ['tests/data/*']}
+    package_data={'mitty': ['tests/data/*']}
 )
