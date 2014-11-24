441cc0e346f4ee03fcf9a0e27282f0f8973afe5c
Björn Pollex <bjoern.pollex@sbgenomics.com>
2014-11-24 11:22:22 +0000

    Updated setup to only specify abstract dependencies

--- setup.py
+++ setup.py
@@ -10,15 +10,12 @@ setup(
     scripts=['mitty/denovo.py', 'mitty/checkbam.py', 'mitty/population.py', 'mitty/reads2bam.py', 'mitty/vcf2reads.py'],
     include_package_data=True,
     package_data={'mitty': ['tests/data/*']},
-    setup_requires=['cython_ext'],
     install_requires=[
-        'setuptools>=7.0',
+        'cython-ext',
         'numpy>=1.9.0',
         'scipy>=0.14.0',
         'docopt>=0.6.2',
         'pysam>=0.8.0',
         'PyVCF'],
-    dependency_links=['git+https://gitlab.sbgenomics.com:9443/bjoern/cython_ext.git#egg=cython_ext-0.1',
-                      'git+https://github.com/jamescasbon/PyVCF.git#egg=PyVCF'],
     cython_ext='mitty/lib/*.pyx'
 )
