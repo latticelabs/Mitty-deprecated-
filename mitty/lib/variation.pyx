b78b3485f85914ff0da3513ddb8bcb380c597f03
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-03-02 11:07:56 -0500

    Code data type refactor
    Variation is now deprecated. Genotype replaces it and carries an index to a master list of VariationData objects instead of a physical pointer.
    
    Tests for variation pass, but these changes now need to be propagated to the rest of the code.

--- mitty/lib/variation.pyx
+++ mitty/lib/variation.pyx
@@ -2,7 +2,6 @@
 genomes (collections of variations). For a description of design choices and algorithms please see the developer
 documentation
 """
-
 # Types of zygosity
 ABSENT = 0
 HOMOZYGOUS = 3
@@ -14,23 +13,133 @@ GT = ['0|0', '0|1', '1|0', '1|1']  # This needs to match rev 3 definitions
 
 cdef class VariationData:
   """VD(pos, stop, REF, ALT)
+  A lightweight class to carry a single variant
+
   Attributes:
-    POS
-    footprint
-    REF
-    ALT"""
+    POS  - position of variant
+    stop - where does footprint of variant on reference (position of last base + 1 in the REF entry)
+    REF  - reference sequence
+    ALT  - alternate sequence"""
   cdef public:
-    long int POS, stop
+    long int POS, stop, hash
     str REF, ALT
 
   def __cinit__(self, int _pos=0, int _stop=0, str _ref='', str _alt=''):
     self.POS, self.stop, self.REF, self.ALT = _pos, _stop, _ref, _alt
+    self.hash = self.POS ^ self.REF.__hash__() ^ self.ALT.__hash__()
+
+  def __hash__(self):
+    return self.hash
+
+
+cdef class Genotype:
+  """Represents a variant genotype in a sample
+  Genotype(index, het)
+  Attributes:
+    index  - position of variant (indexes into master list)
+    het    - zygosity (genotype) information"""
+  cdef public:
+    long int index
+    unsigned char het
+
+  def __cinit__(self, index, het):
+    self.index, self.het = index, het
+
+  def __repr__(self):
+    return '{:d}'.format(self.index)
+
+  cpdef unsigned long pos(self):
+    return self.index >> 16
+
+  cpdef unsigned int alts(self):
+    return self.index & 0xffff
+
+
+cpdef unsigned long add_novel_variant_to_master(vd, l):
+  """Add this new variant to the dictionary. Intelligently handle case where we have an existing allele
+  at the same locus.
+  :param vd: This variant in the form of a VariationData instance
+  :param l: Python dictionary storing all variants in a chromosome
+  :returns index
+  """
+  n = 0
+  index = vd.POS << 16 | n
+  while index in l:  # Find us an empty spot
+    if l[index].hash == vd.hash:
+      return index # This is identical to an existing variant
+    n += 1
+    index = vd.POS << 16 | n
+  l[index] = vd
+  return index
+
+
+cpdef Genotype add_as_genotype(vd, l, het):
+  """Wrapper around add_novel_variant_to_master to yield a properly filled in genotype
+  :param vd: This variant in the form of a VariationData instance
+  :param l: Python dictionary storing all variants in a chromosome
+  :param het: Zygosity (genotype) information
+  :returns Genotype object
+  """
+  index = add_novel_variant_to_master(vd, l)
+  return Genotype(index, het)
+
+
+cpdef list find_next_line(samples, n_samples, csc):
+  """Find the next line
+  :param samples: list of samples
+  :param n_samples: number of sample
+  :param csc: list of cursors pointing to where we are in each sample
+  :returns idx_of_min: a list of indexes (into the sample list) indicating which of them currently form a common line
+  For the current list of sample cursors, find out the ones that form a common line (i.e. share POS, if not REF and ALT)
+  """
+  idx_of_min = []
+  min_pos = samples[0][csc[0]].pos()
+  for n in range(n_samples):
+    if csc[n] is not None:
+      if samples[n][csc[n]].pos() < min_pos:
+        idx_of_min = [n]
+        min_pos = samples[n][csc[n]].pos()
+      elif samples[n][csc[n]].pos() == min_pos:
+        idx_of_min += [n]
+  return idx_of_min
+
+
+# def find_alternate_alleles(samples, n_samples, this_line):
+#   """Given a line, see if there are alternate alleles which we should collapse.
+#   :param samples: list of samples
+#   :param n_samples: number of sample
+#   :param this_line:
+#   """
+#   for el in this_line:
+
+
+def get_variant_rows(l, samples):
+  """Iterator to write out the collection of samples in multi-sample format
+  :param l: the master variant dictionary
+  :param samples: list of samples (each of which is a list of Genotype structures)
+  """
+  n_samples = len(samples)
+  msi = [len(s) for s in samples]  # Maximum sample index
+  csc = [0 if msi[n] else None for n in range(n_samples)]  # Current sample cursor
+
+  while len([i for i in csc if i is not None]):
+    this_line = find_next_line(samples, n_samples, csc)
+    yield this_line
+    for n in this_line:  # Now move all cursors forward
+      csc[n] += 1
+      if csc[n] == msi[n]:
+        csc[n] = None
+
+
+def add_sample_variant(gt, l):
+  pass
 
 
 def new_variation(int _pos=0, int _stop=0, str _ref='', str _alt='', char het=0, char recessive=0, float fitness=0):
   return Variation(VariationData(_pos, _stop, _ref, _alt), het, recessive, fitness)
 
 
+# Will be deprecated in favor of Genotype
 cdef class Variation:
   """Carries *reference* to a VariationData instance and heterozygosity, recessiveness and fitness data."""
   cdef public:
@@ -44,8 +153,8 @@ cdef class Variation:
 
   # Need this for set to work (https://docs.python.org/2/reference/datamodel.html#object.__hash__)
   def  __hash__(self):
-    return self.vd.POS ^ self.vd.REF.__hash__() ^ self.vd.ALT.__hash__()
-
+    #return self.vd.POS ^ self.vd.REF.__hash__() ^ self.vd.ALT.__hash__()
+    return self.vd.hash
 
   cpdef bint eq(self, Variation other):
     # We don't consider zygosity, fitness or recessiveness
@@ -67,25 +176,69 @@ cdef class Variation:
       format(self.vd.POS, self.vd.stop, self.vd.REF, self.vd.ALT, GT[self.het], self.recessive, self.fitness)
 
 
-def copy_variant_sequence(list c1, idx=None, het=None):
-  """copy_variant_sequence(list c1, idx, zygosity)
-  :param list c1: a sequence of variants that need to be copied over.
-  :rtype generator: Generated copy of the variant list. Note that we always share the VariantData"""
-  cdef Variation v1
-  cdef int n
-  if idx is None:
-    return (Variation(v1.vd, v1.het, v1.recessive, v1.fitness) for v1 in c1)
-  elif het is None:
-    return (Variation(c1[n].vd, c1[n].het, c1[n].recessive, c1[n].fitness) for n in idx)
-  else:
-    return (Variation(c1[n].vd, het[n2], c1[n].recessive, c1[n].fitness) for n2, n in enumerate(idx))
+# def copy_variant_sequence(list c1, idx=None, het=None):
+#   """copy_variant_sequence(list c1, idx, zygosity)
+#   :param list c1: a sequence of variants that need to be copied over.
+#   :rtype generator: Generated copy of the variant list. Note that we always share the VariantData"""
+#   cdef Variation v1
+#   cdef int n
+#   if idx is None:
+#     return (Variation(v1.vd, v1.het, v1.recessive, v1.fitness) for v1 in c1)
+#   elif het is None:
+#     return (Variation(c1[n].vd, c1[n].het, c1[n].recessive, c1[n].fitness) for n in idx)
+#   else:
+#     return (Variation(c1[n].vd, het[n2], c1[n].recessive, c1[n].fitness) for n2, n in enumerate(idx))
 
 
-cdef inline Variation copy_variant(Variation v1):
-  return Variation(v1.vd, v1.het, v1.recessive, v1.fitness)
+# cdef inline Variation copy_variant(Variation v1):
+#   return Variation(v1.vd, v1.het, v1.recessive, v1.fitness)
 
 
-def merge_variants(c1, c2):
+# def merge_variants(c1, c2):
+#   """merge_variants(c1, c2)
+#   Given an existing chromosome (list of variants) merge a new list of variants into it in zipper fashion. c1 has
+#   priority (collisions are resolved in favor of c1)
+#
+#   :param c1: The original variants (iterable)
+#   :param c2: The proposed new variants (iterable)
+#   :rtype list: The resultant variant list with collisions arbitrated
+#
+#   Algorithm::
+#
+#     o(x,y) = True if x and y overlap
+#            = False otherwise
+#     c1 = existing list of variants
+#     c2 = denovo list of variants
+#     c3 = new list of variants being built
+#
+#     o(c1, c2) = True: add(c1) c1++, c2++
+#               = False:
+#                 c1 < c2 ? add(c1) c1++
+#                 else:
+#                   o(c3, c2) = True: c2++
+#                             = False: add(c2), c2++
+#
+#   """
+#   return c_merge_variants(c1, c2)
+
+
+cdef inline bint overlap(VariationData v1, unsigned char het1, VariationData v2, unsigned char het2):
+  """Returns true if the footprints of the variations overlap.
+  :param v1: first variant
+  :param het1: genotype of first variant
+  :param v2: second variant
+  :param het2: genotype of second variant
+  :returns Bool
+  """
+  if v2.POS - 1 <= v1.POS <= v2.stop + 1 or v2.POS - 1 <= v1.stop <= v2.stop + 1 or \
+                  v1.POS <= v2.POS - 1 <= v2.stop + 1 <= v1.stop:  # Potential overlap
+    if het1 == het2 or het1 == HOMOZYGOUS or het2 == HOMOZYGOUS:  # Definite overlap
+      return True
+  return False
+
+
+#TODO: refactor this to be faster? Not use copy_variant? Code can be made cleaner
+cpdef merge_variants(list c1, list c2, dict ml):
   """merge_variants(c1, c2)
   Given an existing chromosome (list of variants) merge a new list of variants into it in zipper fashion. c1 has
   priority (collisions are resolved in favor of c1)
@@ -110,48 +263,35 @@ def merge_variants(c1, c2):
                             = False: add(c2), c2++
 
   """
-  return c_merge_variants(c1, c2)
-
-
-cdef inline bint overlap(Variation x, Variation y):
-  # Returns true if the footprints of the variations overlap.
-  if x is None or y is None: return False
-  if y.vd.POS - 1 <= x.vd.POS <= y.vd.stop + 1 or y.vd.POS - 1 <= x.vd.stop <= y.vd.stop + 1 or \
-                  x.vd.POS <= y.vd.POS - 1 <= y.vd.stop + 1 <= x.vd.stop:  # Potential overlap
-    if x.het == y.het or x.het == HOMOZYGOUS or y.het == HOMOZYGOUS:  # Definite overlap
-      return True
-  return False
-
-
-#TODO: refactor this to be faster? Not use copy_variant? Code can be made cleaner
-cdef c_merge_variants(list c1, list c2):
   cdef:
     int n1_max = len(c1), n2_max = len(c2)
     int n1 = 0, n2 = 0, n3 = 0
+    VariationData v1, v2
   c3 = [None] * (n1_max + n2_max)  # This is the maximum size of c3
   while n1 < n1_max and n2 < n2_max:
-    if overlap(c1[n1], c2[n2]): # This will collide. Advance c2 and redo. Fixes case for merge_test8
+    v1, v2 = ml[c1[n1].index], ml[c2[n2].index]
+    if overlap(v1, c1[n1].het, v2, c2[n2].het): # This will collide. Advance c2 and redo. Fixes case for merge_test8
       n2 += 1
     else:  # No collision
-      if c1[n1].vd.POS <= c2[n2].vd.POS:  # Zip-in c1 as it comes before c2
-        c3[n3] = copy_variant(c1[n1])
+      if v1.POS <= v2.POS:  # Zip-in c1 as it comes before c2
+        c3[n3] = c1[n1]
         n1 += 1
         n3 += 1
       else:  # Zip-in c2 as it comes before next c1
-        if n3==0 or not overlap(c3[n3 - 1], c2[n2]):
-          c3[n3] = copy_variant(c2[n2])
+        if n3==0 or not overlap(ml[c3[n3 - 1].index], c3[n3 - 1].het, v2, c2[n2].het):
+          c3[n3] = c2[n2]
           n3 += 1
         n2 +=1  # Need to advance c2 anyway
 
   # Now copy over slack
   while n1 < n1_max:  # Smooth sailing, just copy over the rest of the original (c1)
-    c3[n3] = copy_variant(c1[n1])
+    c3[n3] = c1[n1]
     n1 += 1
     n3 += 1
 
   while n2 < n2_max:  # Need to test each new (c2) for clashes with itself
-    if n3==0 or not overlap(c3[n3 - 1], c2[n2]):
-      c3[n3] = copy_variant(c2[n2])
+    if n3==0 or not overlap(ml[c3[n3 - 1].index], c3[n3 - 1].het, ml[c2[n2].index], c2[n2].het):
+      c3[n3] = c2[n2]
       n3 += 1
     n2 += 1  # Need to advance c2 anyway
 
@@ -167,20 +307,33 @@ def copy_missing_chromosomes(g1, g2):
   missing_chrom = set(g2.keys()) - set(g1.keys())
   cdef int ch
   for ch in missing_chrom:
-    g1[ch] = list(copy_variant_sequence(g2[ch]))
+    g1[ch] = list(g2[ch])
 
 
-def merge_genomes(g1={}, g2={}):
+def merge_genomes(dict g1, dict g2, dict ml):
   """Given two genomes run merge_variants(c1, c2) on each chromosome. g1 has priority. In the end copy any chromosomes
   present in g2 but not in g1 into g1 (so that g1 is a superset of g2)
 
   :param dict g1: genome
   :param dict g2: genome
+  :param dict ml: master variation list
   :returns: new genome
   """
   g3 = {}
   for chrom, c2 in g2.iteritems():
-    g3[chrom] = merge_variants(g1.get(chrom, []), c2)
+    g3[chrom] = merge_variants(g1.get(chrom, []), c2, ml)
   copy_missing_chromosomes(g3, g1)  # The previous loop merges all chr in c2.
                                     # Now, we need to consider all chr in c1 but not in c2
   return g3
+
+
+"""Variants kept as a dictionary with hash as (pos, allele). This should be Cythonized
+
+The pos is hashed in the following way. We store the POS as the upper 28 bytes of a 32 byte int. The remaining 4 bytes
+are used, if needed, to indicate minor allele serial for variations that have the same POS.
+
+The way the list works is that when a novel variant is added to a sample, we add it to the master dictionary, get back an
+index and then add the Genotype to the
+
+
+"""
--- mitty/tests/lib/variation_test.py
+++ mitty/tests/lib/variation_test.py
@@ -2,157 +2,236 @@ import mitty.lib.variation as vr
 from mitty.lib.variation import HOMOZYGOUS, HET_01, HET_10
 from nose.tools import assert_sequence_equal
 
+VD = vr.VariationData
 nv = vr.new_variation
+gt = vr.Genotype
+HH = HOMOZYGOUS
 
 
-def copy_test1():
-  """Copy list of variants with no modification"""
-  c1 = [v0, v1] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS), nv(13, 16, 'CTT', 'C', HOMOZYGOUS)]
-  c2 = vr.copy_variant_sequence(c1)
-  assert_sequence_equal(c1, list(c2))
+def addition_test():
+  """Add variant to master list"""
+  l = {}
+  index = vr.add_novel_variant_to_master(VD(1, 2, 'A', 'C'), l)
+  assert index == 65536
 
 
-def copy_test2():
-  """Copy list of variants selectively"""
-  c1 = [v0, v1, v2] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS),
-                       nv(7, 8, 'C', 'T', HOMOZYGOUS),
-                       nv(13, 16, 'CTT', 'C', HOMOZYGOUS)]
-  c2 = vr.copy_variant_sequence(c1, idx=[0, 2])
-  assert_sequence_equal([v0, v2], list(c2))
+def addition_test2():
+  """Duplicate variant in master list"""
+  l = {}
+  index = vr.add_novel_variant_to_master(VD(1, 2, 'A', 'C'), l)
+  assert index == 65536
+  index = vr.add_novel_variant_to_master(VD(1, 2, 'A', 'C'), l)
+  assert index == 65536
 
 
-def copy_test3():
-  """Copy list of variants selectively with zygosity"""
-  c1 = [v0, v1, v2] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS),
-                       nv(7, 8, 'C', 'T', HOMOZYGOUS),
-                       nv(13, 16, 'CTT', 'C', HOMOZYGOUS)]
-  c2 = vr.copy_variant_sequence(c1, idx=[0, 2], het=[HET_01, HET_10])
-  v0d = vr.Variation(v0.vd, het=HET_01)
-  v2d = vr.Variation(v2.vd, het=HET_10)
-  assert_sequence_equal([v0d, v2d], list(c2))
+def addition_test3():
+  """Multiple allele detection on addition to master list"""
+  l = {}
+  index = vr.add_novel_variant_to_master(VD(1, 2, 'A', 'C'), l)
+  assert index == 65536
+  index = vr.add_novel_variant_to_master(VD(1, 2, 'A', 'G'), l)
+  assert index == 65537
+
+
+def cgt(l, pos, stop=None, ref='A', alt='C', het=HOMOZYGOUS):
+  vd = VD(pos, stop or pos + 1, ref, alt)
+  return vr.add_as_genotype(vd, l, het)
+
+
+def find_next_line_test():
+  """Multiple sample line from master list"""
+  l = {}
+  g = [cgt(l, n + 1, HH) for n in range(8)]
+  s1 = [g[0], g[2], g[6]]
+  s2 = [g[0], g[1], g[3], g[7]]
+  s3 = [g[1], g[4], g[5], g[6]]
+
+  samples = [s1, s2, s3]
+
+  csc = [0, 0, 0]
+  idx_of_min = vr.find_next_line(samples, 3, csc)
+  assert idx_of_min == [0, 1], idx_of_min
+
+  csc = [1, 1, 0]
+  idx_of_min = vr.find_next_line(samples, 3, csc)
+  assert idx_of_min == [1, 2], idx_of_min
+
+  csc = [1, 2, 1]
+  idx_of_min = vr.find_next_line(samples, 3, csc)
+  assert idx_of_min == [0], idx_of_min
+
+
+def multi_sample_test():
+  """Multiple sample iterator from master list"""
+  l = {}
+  g = [cgt(l, n + 1, HH) for n in range(8)]
+  s1 = [g[0], g[2], g[6]]
+  s2 = [g[0], g[1], g[3], g[7]]
+  s3 = [g[1], g[4], g[5], g[6]]
+
+  samples = [s1, s2, s3]
+  gvr = vr.get_variant_rows(l, samples)
+  correct_rows = [[0, 1], [1, 2], [0]]
+
+  for row, crow in zip(gvr, correct_rows):
+    assert row == crow, row
+
+
+# def copy_test1():
+#   """Copy list of variants with no modification"""
+#   c1 = [v0, v1] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS), nv(13, 16, 'CTT', 'C', HOMOZYGOUS)]
+#   c2 = vr.copy_variant_sequence(c1)
+#   assert_sequence_equal(c1, list(c2))
+#
+#
+# def copy_test2():
+#   """Copy list of variants selectively"""
+#   c1 = [v0, v1, v2] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS),
+#                        nv(7, 8, 'C', 'T', HOMOZYGOUS),
+#                        nv(13, 16, 'CTT', 'C', HOMOZYGOUS)]
+#   c2 = vr.copy_variant_sequence(c1, idx=[0, 2])
+#   assert_sequence_equal([v0, v2], list(c2))
+#
+#
+# def copy_test3():
+#   """Copy list of variants selectively with zygosity"""
+#   c1 = [v0, v1, v2] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS),
+#                        nv(7, 8, 'C', 'T', HOMOZYGOUS),
+#                        nv(13, 16, 'CTT', 'C', HOMOZYGOUS)]
+#   c2 = vr.copy_variant_sequence(c1, idx=[0, 2], het=[HET_01, HET_10])
+#   v0d = vr.Variation(v0.vd, het=HET_01)
+#   v2d = vr.Variation(v2.vd, het=HET_10)
+#   assert_sequence_equal([v0d, v2d], list(c2))
 
 
 def merge_test1():
   """Merge variants, non overlapping, existing first (ED)."""
-  c1 = [v10] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS)]
-  c2 = [v20] = [nv(10, 13, 'CAA', 'C', HOMOZYGOUS)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c1 = [v10] = [cgt(l, 1, 4, 'CAA', 'C', HOMOZYGOUS)]
+  c2 = [v20] = [cgt(l, 10, 13, 'CAA', 'C', HOMOZYGOUS)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v10, v20])
 
 
 def merge_test2():
   """Merge variants, non overlapping, denovo first (DE)."""
-  c1 = [v10] = [nv(10, 13, 'CAA', 'C', HOMOZYGOUS)]
-  c2 = [v20] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c1 = [v10] = [cgt(l, 10, 13, 'CAA', 'C', HOMOZYGOUS)]
+  c2 = [v20] = [cgt(l, 1, 4, 'CAA', 'C', HOMOZYGOUS)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v20, v10])
 
 
 def merge_test3():
   """Merge variants, non overlapping (EDED)"""
-  c1 = [v10, v11] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS), nv(13, 16, 'CTT', 'C', HOMOZYGOUS)]
-  c2 = [v20, v21] = [nv(8, 11, 'CCC', 'C', HOMOZYGOUS), nv(20, 23, 'CAA', 'C', HOMOZYGOUS)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c1 = [v10, v11] = [cgt(l, 1, 4, 'CAA', 'C', HOMOZYGOUS), cgt(l, 13, 16, 'CTT', 'C', HOMOZYGOUS)]
+  c2 = [v20, v21] = [cgt(l, 8, 11, 'CCC', 'C', HOMOZYGOUS), cgt(l, 20, 23, 'CAA', 'C', HOMOZYGOUS)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v10, v20, v11, v21])
 
 
 def merge_test4a():
   """Merge variants, full overlapping (E-D-)"""
-  c1 = [v10] = [nv(2, 5, 'CAA', 'C', HOMOZYGOUS)]
-  c2 = [v20] = [nv(2, 5, 'CAA', 'T', HOMOZYGOUS)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c1 = [v10] = [cgt(l, 2, 5, 'CAA', 'C', HOMOZYGOUS)]
+  c2 = [v20] = [cgt(l, 2, 5, 'CAA', 'T', HOMOZYGOUS)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v10])
 
 
 def merge_test4b():
   """Merge variants, full overlapping, SNP (D-E-)."""
-  c1 = [v10] = [nv(2, 3, 'C', 'G', HET_10)]
-  c2 = [v20] = [nv(2, 3, 'C', 'T', HET_10)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c1 = [v10] = [cgt(l, 2, 3, 'C', 'G', HET_10)]
+  c2 = [v20] = [cgt(l, 2, 3, 'C', 'T', HET_10)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v10])
 
 
 # Important test - killed a nasty logic bug
 def merge_test4c():
   """Merge variants, full overlapping, with non-colliding preceder"""
-  c1 = [v10, v11] = [nv(1, 2, 'G', 'C', HET_01),
-                     nv(2, 3, 'A', 'C', HET_10)]
-  c2 = [v20] = [nv(2, 3, 'A', 'T', HET_10)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c1 = [v10, v11] = [cgt(l, 1, 2, 'G', 'C', HET_01),
+                     cgt(l, 2, 3, 'A', 'C', HET_10)]
+  c2 = [v20] = [cgt(l, 2, 3, 'A', 'T', HET_10)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v10, v11])
 
 
 def merge_test4():
   """Merge variants, overlapping (E-D). D will collide and will be rejected"""
-  c1 = [v10] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS)]
-  c2 = [v20] = [nv(2, 5, 'CCC', 'C', HOMOZYGOUS)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c1 = [v10] = [cgt(l, 1, 4, 'CAA', 'C', HOMOZYGOUS)]
+  c2 = [v20] = [cgt(l, 2, 5, 'CCC', 'C', HOMOZYGOUS)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v10])
 
 
 def merge_test5():
   """Merge variants, overlapping (D-E). D will collide and will be rejected"""
-  c2 = [v20] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS)]
-  c1 = [v10] = [nv(2, 5, 'CCC', 'C', HOMOZYGOUS)]
-  c3 = vr.merge_variants(c1, c2)
+  l = {}
+  c2 = [v20] = [cgt(l, 1, 4, 'CAA', 'C', HOMOZYGOUS)]
+  c1 = [v10] = [cgt(l, 2, 5, 'CCC', 'C', HOMOZYGOUS)]
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal(c3, [v10])
 
 
 def merge_test6():
   """Merge variants, overlapping heterozygous. Overlapping but with mixed zygosity"""
-  c1 = [v10, v11, v12, v13] = [nv(1, 4, 'CAA', 'C', HOMOZYGOUS),
-                               nv(13, 16, 'CAA', 'C', HET_10),
-                               nv(20, 23, 'CAA', 'C', HET_10),
-                               nv(26, 29, 'CAA', 'C', HOMOZYGOUS)]
-  c2 = [v20, v21, v22, v23] = [nv(5, 8, 'CTT', 'C', HOMOZYGOUS),  # Will collide out
-                               nv(13, 16, 'CTT', 'C', HET_01),  # Will pass
-                               nv(20, 23, 'CTT', 'C', HOMOZYGOUS),  # Will collide out
-                               nv(26, 29, 'CTT', 'C', HET_01)]  # Will collide out
-  c3 = vr.merge_variants(c1, c2)
-  # assert_sequence_equal(c2, [new_variation(1, 4, 'CAA', 'C', HOMOZYGOUS),
-  #                   new_variation(13, 16, 'CTT', 'C', HET_10),
-  #                   new_variation(13, 16, 'CAA', 'C', HET_01),
-  #                   new_variation(20, 23, 'CAA', 'C', HET_10),
-  #                   new_variation(26, 29, 'CGG', 'C', HOMOZYGOUS)])
+  l = {}
+  c1 = [v10, v11, v12, v13] = [cgt(l, 1, 4, 'CAA', 'C', HOMOZYGOUS),
+                               cgt(l, 13, 16, 'CAA', 'C', HET_10),
+                               cgt(l, 20, 23, 'CAA', 'C', HET_10),
+                               cgt(l, 26, 29, 'CAA', 'C', HOMOZYGOUS)]
+  c2 = [v20, v21, v22, v23] = [cgt(l, 5, 8, 'CTT', 'C', HOMOZYGOUS),  # Will collide out
+                               cgt(l, 13, 16, 'CTT', 'C', HET_01),  # Will pass
+                               cgt(l, 20, 23, 'CTT', 'C', HOMOZYGOUS),  # Will collide out
+                               cgt(l, 26, 29, 'CTT', 'C', HET_01)]  # Will collide out
+  c3 = vr.merge_variants(c1, c2, l)
   assert_sequence_equal([v10, v11, v21, v12, v13], c3)
 
 
 def merge_test7():
   """Skip bug, discovered during simulations"""
-  cv1 = nv(3, 7, 'ACTG', 'A', HOMOZYGOUS)
-  cv2 = nv(10, 11, 'C', 'A', HOMOZYGOUS)
+  l = {}
+  cv1 = cgt(l, 3, 7, 'ACTG', 'A', HOMOZYGOUS)
+  cv2 = cgt(l, 10, 11, 'C', 'A', HOMOZYGOUS)
   c1 = [cv1, cv2]
 
-  dv1 = nv(1, 5, 'ACTG', 'A', HOMOZYGOUS)
-  dv2 = nv(7, 8, 'C', 'T', HOMOZYGOUS)  # Collides with previous, should be discarded
-  dv3 = nv(15, 16, 'A', 'T', HOMOZYGOUS)  # Too close together, second one should collide out
-  dv4 = nv(16, 16, 'C', 'T', HOMOZYGOUS)
+  dv1 = cgt(l, 1, 5, 'ACTG', 'A', HOMOZYGOUS)
+  dv2 = cgt(l, 7, 8, 'C', 'T', HOMOZYGOUS)  # Collides with previous, should be discarded
+  dv3 = cgt(l, 15, 16, 'A', 'T', HOMOZYGOUS)  # Too close together, second one should collide out
+  dv4 = cgt(l, 16, 16, 'C', 'T', HOMOZYGOUS)
   dnv = [dv1, dv2, dv3, dv4]
 
-  c2 = vr.merge_variants(c1, dnv)
+  c2 = vr.merge_variants(c1, dnv, l)
   assert_sequence_equal(c2, [cv1, cv2, dv3])
 
 
 def merge_test8():
   """Tricky variant merge case that led to out of order result"""
-  cv1 = nv(26, 27, 'A', 'ACA', HET_01)
+  l = {}
+  cv1 = cgt(l, 26, 27, 'A', 'ACA', HET_01)
   c1 = [cv1]
 
-  dv1 = nv(24, 25, 'A', 'C', HET_01)
-  dv2 = nv(25, 26, 'C', 'A', HET_10)
+  dv1 = cgt(l, 24, 25, 'A', 'C', HET_01)
+  dv2 = cgt(l, 25, 26, 'C', 'A', HET_10)
   dnv = [dv1, dv2]
 
-  c2 = vr.merge_variants(c1, dnv)
+  c2 = vr.merge_variants(c1, dnv, l)
   assert_sequence_equal(c2, [dv2, cv1], c2)
 
 
 def copy_missing_chromosomes_test():
   """Copy missing chromosomes."""
-  c1 = [v10, v11] = [nv(1, 2, 'A', 'T', HOMOZYGOUS),
-                     nv(4, 5, 'C', 'A', HOMOZYGOUS)]
+  l = {}
+  c1 = [v10, v11] = [cgt(l, 1, 2, 'A', 'T', HOMOZYGOUS),
+                     cgt(l, 4, 5, 'C', 'A', HOMOZYGOUS)]
   g1 = {1: c1}
-  c2 = [v20, c21] = [nv(3, 4, 'A', 'T', HOMOZYGOUS),
-                     nv(6, 7, 'C', 'A', HOMOZYGOUS)]
+  c2 = [v20, c21] = [cgt(l, 3, 4, 'A', 'T', HOMOZYGOUS),
+                     cgt(l, 6, 7, 'C', 'A', HOMOZYGOUS)]
   g2 = {2: c2}
   vr.copy_missing_chromosomes(g1, g2)
   assert_sequence_equal(g1[1], c1)
@@ -161,12 +240,13 @@ def copy_missing_chromosomes_test():
 
 def merge_genomes_test():
   """Merge genomes."""
-  c1 = [v10, v11] = [nv(1, 2, 'A', 'T', HOMOZYGOUS),
-                     nv(4, 5, 'C', 'A', HOMOZYGOUS)]
+  l = {}
+  c1 = [v10, v11] = [cgt(l, 1, 2, 'A', 'T', HOMOZYGOUS),
+                     cgt(l, 4, 5, 'C', 'A', HOMOZYGOUS)]
   g1 = {1: c1}
-  c2 = [v20, c21] = [nv(3, 4, 'A', 'T', HOMOZYGOUS),
-                     nv(6, 7, 'C', 'A', HOMOZYGOUS)]
+  c2 = [v20, c21] = [cgt(l, 3, 4, 'A', 'T', HOMOZYGOUS),
+                     cgt(l, 6, 7, 'C', 'A', HOMOZYGOUS)]
   g2 = {1: c1, 2: c2}
-  g3 = vr.merge_genomes(g1, g2)
+  g3 = vr.merge_genomes(g1, g2, l)
   assert_sequence_equal(g3[1], c1)
   assert_sequence_equal(g3[2], c2)
