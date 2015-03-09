f4105b6bb32b0341c6e04a6dd7e71630a0a12e59
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-03-09 15:20:15 -0400

    Modified for new, simpler, plugin interface

--- mitty/plugins/variants/snp_plugin.py
+++ mitty/plugins/variants/snp_plugin.py
@@ -1,16 +1,15 @@
 """This is the stock SNP plugin. It uses four independent RNGs to locate SNPs along a reference sequence, assign each
 SNP a zygosity and assign an ALT base.
 """
-from mitty.lib.variation import new_variation
 import mitty.lib.util as mutil
 import logging
 logger = logging.getLogger(__name__)
 
 __example_param_text = """
 {
-  "chromosome": [4],      # List of chromosomes to apply the variant to
+  "chromosome": [4],      # List of chromosomes to apply the data to
   "p": 0.01,              # probability that the SNP will happen at any given base
-  "phet": 0.5,            # probability that the variant will be heterozygous
+  "phet": 0.5,            # probability that the data will be heterozygous
   "t_mat": [[ 0.32654629,  0.17292732,  0.24524503,  0.25528135],  # Base transition matrix
             [ 0.3489394,   0.25942695,  0.04942584,  0.3422078],   # Leading diagonal is ignored
             [ 0.28778188,  0.21087004,  0.25963262,  0.24171546],
@@ -48,14 +47,12 @@ def variant_generator(ref={},
     snp_locs = [x for x in mutil.place_poisson(base_loc_rng, p, len(ref_chrom)) if ref_chrom[x] != 'N']
     het_type = mutil.zygosity(len(snp_locs), phet, het_rng, copy_rng)
     base_subs = mutil.base_subs(ref_chrom, snp_locs, t_mat, base_t_rng)
-    yield {chrom: [new_variation(pos + 1, pos + 2, ref_chrom[pos], bs, het)
-                   for pos, bs, het in zip(snp_locs, base_subs, het_type)]}
+    yield {chrom: [[pos + 1 for pos in snp_locs], [ref_chrom[pos] for pos in snp_locs], base_subs, het_type]}
     # +1 because VCF files are 1 indexed
     #alts will be 0 if ref is not one of ACTG
 
 
 def test():
-  from mitty.lib.variation import Variation
   """Basic test"""
   ref = {
     1: 'ACTGACTGACTG',
@@ -63,7 +60,10 @@ def test():
   }
   vg = variant_generator(ref, chromosome=[1, 2], p=1.0)
   for v_list in vg:
-    assert isinstance(v_list.values()[0][0], Variation), v_list
+    for chrom, dnv in v_list.iteritems():
+      for p, r, a, gt in zip(*dnv):
+        assert ref[chrom][p - 1] == r
+
 
 if __name__ == "__main__":
   print _description
