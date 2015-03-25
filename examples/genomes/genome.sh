900f3441653ba04bce8e12cae7917d8e113c5abb
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-03-25 10:07:57 -0400

    Cleaning up examples

--- examples/genomes/genome.sh
+++ examples/genomes/genome.sh
@@ -13,3 +13,14 @@ genomes dryrun --pfile=chimera_snp.json
 : Generate mutations
 genomes generate --pfile=chimera_snp.json -v
 
+: Inspect the file
+genomes inspect --dbfile=Out/chimera.db
+
+: Write out a VCF from the file
+genomes write vcf 0 --dbfile=Out/chimera.db
+
+: Write out two VCFs from the file
+genomes write vcf 1 2 --dbfile=Out/chimera.db
+
+: Write out all VCFs from the file '(How large is your disk?)'
+genomes write vcf --dbfile=Out/chimera.db
