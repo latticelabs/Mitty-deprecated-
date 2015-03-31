b2ad99f22a3dcfe8f23542267e82f7e36de430f9
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-03-30 23:03:19 -0400

    Partial attempt using state machine
    Last commit before trying out general equation method

--- mitty/lib/reads.py
+++ mitty/lib/reads.py
@@ -40,8 +40,83 @@ def expand_sequence(ref_seq, ml, chrom, copy):
       c = next(c_iter, None)
   alt_fragments += [ref_seq[pos_ref:]]
   variant_waypoint += [(2 ** 31 - 1, 2 ** 31 - 1, 2 ** 31 - 1)]  # The end waypoint, guaranteed to be to the right of any base
-  dtype = [('ref_pos', 'i4'), ('alt_pos', 'i4'), ('stop', 'i4')]
+  dtype = [('ref_pos', 'i4'), ('alt_pos', 'i4'), ('delta', 'i4')]
   return ''.join(alt_fragments), np.rec.fromrecords(variant_waypoint, dtype=dtype)
+
+
+pref = lambda x, s: str(x) + s if x > 0 else ''
+
+state_machine = {
+  'b': {
+    'S': lambda l, k, x: pref(l - 1, 'M') + '1X',
+    'D': lambda l, k, x: pref(l, 'M'),
+    'I': lambda l, k, x: pref(l, 'M'),
+    'e': lambda l, k, x: pref(l, 'M')
+  },
+  'bI': {
+    'S': lambda l, k, x: pref(k, 'S') + pref(l - 1 - k, 'M') + '1X',
+    'D': lambda l, k, x: pref(k, 'S') + pref(l - k, 'M'),
+    'I': lambda l, k, x: pref(k, 'S') + pref(l - k, 'M'),
+    'e': lambda l, k, x: pref(k, 'S') + pref(l - k, 'M')
+  },
+  'S': {
+    'S': lambda l, k, x: pref(l - 1, 'M') + '1X',
+    'D': lambda l, k, x: pref(l, 'M'),
+    'I': lambda l, k, x: pref(l, 'M'),
+    'e': lambda l, k, x: pref(l, 'M')
+  },
+  'D' : {
+    'S': lambda l, k, x: pref(x, 'D') + pref(l - 1, 'M') + '1X',
+    'D': lambda l, k, x: pref(x, 'D') + pref(l, 'M'),
+    'I': lambda l, k, x: pref(x, 'D') + pref(l, 'M'),
+    'e': lambda l, k, x: pref(x, 'D') + pref(l, 'M')
+  },
+  'I': {
+    'S': lambda l, k, x: pref(k, 'I') + pref(l - 1 - k, 'M') + '1X',
+    'D': lambda l, k, x: pref(k, 'I') + pref(l - k, 'M'),
+    'I': lambda l, k, x: pref(k, 'I') + pref(l - k, 'M'),
+    'e': lambda l, k, x: pref(k, 'I') + pref(l - k, 'M'),
+    'eI': lambda l, k, x: pref(l, 'S')
+  }
+}
+
+
+def roll_cigars(variant_waypoints, reads):
+  """Use beacons to generate POS and CIGAR strings for reads
+
+  :param variant_waypoints: recarray, as returned by expand_sequence (pos_ref, pos_alt, delta)
+  :param reads: numpy recarray with fields 'start_a' and 'read_len'
+  :return: list of CIGAR strings same length as reads array
+  """
+  vw_r, vw_a, dl = variant_waypoints['ref_pos'], variant_waypoints['alt_pos'], variant_waypoints['delta']
+  st_a, rd_len = reads['start_a'], reads['read_len']
+  beacon_to_right = np.searchsorted(vw_a, st_a)
+  cigars = []
+  for n in range(reads.shape[0]):
+    this_cigar = ''
+    b1 = beacon_to_right[n]
+    curr_pos = st_a[n]
+    read_stop = st_a[n] + rd_len[n]
+    state = 'b' if vw_a[b1 - 1] + dl[b1 - 1] < curr_pos else 'bI'
+    while state != 'e' and state != 'eI':
+      print state, curr_pos
+      l = min(vw_a[b1] - curr_pos, rd_len[n])
+      k = vw_a[b1 - 1] + dl[b1 - 1] - curr_pos
+      x = -dl[b1 - 1]
+      if vw_a[b1] > read_stop:
+        new_state = 'eI' if state == 'I' else 'e'
+      elif dl[b1] == 0:
+        new_state = 'S'
+      elif dl[b1] < 0:
+        new_state = 'D'
+      else:
+        new_state = 'I'
+      this_cigar += state_machine[state][new_state](l, k, x)
+      curr_pos = vw_a[b1]
+      state = new_state
+      b1 += 1
+    cigars += [this_cigar]
+  return cigars
 
 
 from ctypes import *
