b36af0c611ef42680505208097ba2946c5b6377e
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-04-01 09:10:46 -0400

    CIGAR generator fully functional
    Comprehensive test suite passing!

--- mitty/tests/lib/reads_test.py
+++ mitty/tests/lib/reads_test.py
@@ -6,7 +6,7 @@ from nose.tools import assert_sequence_equal
 
 
 def expand_seq_test1():
-  """Sequence expand no variants"""
+  """Expand sequence: no variants"""
   ref_seq = 'ACTGACTGACTGACT'
   ml = vr.VariantList()
   chrom = []
@@ -17,7 +17,7 @@ def expand_seq_test1():
 
 
 def expand_seq_test2():
-  """Sequence expand one variant of each type"""
+  """Expand sequence: one variant of each type"""
   #          012345678901234
   ref_seq = 'ACTGACTGACTGACT'
 
@@ -73,7 +73,7 @@ def expand_seq_test2():
 
 
 def expand_seq_test3():
-  """Sequence expand multiple variants, different combinations"""
+  """Expand sequence: multiple variants, different combinations"""
   #          123456789012
   ref_seq = 'ACTGACTGACTG'
 
@@ -118,8 +118,8 @@ def expand_seq_test3():
 
 def cigar_test1():
   """Rolling cigars: No variants"""
-  #          012345678901
-  ref_seq = 'ACTGACTGACTG'
+  #          012345678901234
+  ref_seq = 'ACTGACTGACTGACT'
   ml = vr.VariantList()
   chrom = []
   alt_seq, variant_waypoints = reads.expand_sequence(ref_seq, ml, chrom, 0)
@@ -131,8 +131,8 @@ def cigar_test1():
 
 def cigar_test2():
   """Rolling cigars: SNP"""
-  #          012345678901
-  ref_seq = 'ACTGACTGACTG'
+  #          012345678901234
+  ref_seq = 'ACTGACTGACTGACT'
   #             ^
   pos = [3]
   stop = [4]
@@ -158,8 +158,8 @@ def cigar_test2():
 
 
 def cigar_test3():
-  """Rolling cigars: INS"""
-  ref_seq = 'ACTGACTGACTG'
+  """Rolling cigars: INS (incl. soft-clip)"""
+  ref_seq = 'ACTGACTGACTGACT'
   pos = [3]
   stop = [4]
   ref = ['G']
@@ -185,7 +185,7 @@ def cigar_test3():
 
 def cigar_test4():
   """Rolling cigars: read completely inside INS"""
-  ref_seq = 'ACTGACTGACTG'
+  ref_seq = 'ACTGACTGACTGACT'
   pos = [3]
   stop = [4]
   ref = ['G']
@@ -208,3 +208,59 @@ def cigar_test4():
   assert cigars[4] == '3S', cigars[4]
   assert cigars[5] == '3S', cigars[5]
   assert cigars[6] == '2S1M', cigars[6]
+
+
+def cigar_test5():
+  """Rolling cigars: DEL (incl. start at deletion)"""
+  ref_seq = 'ACTGACTGACTGACT'
+  pos = [3]
+  stop = [6]
+  ref = ['GAC']
+  alt = ['G']
+  p = [0.1]
+  ml = vr.VariantList(pos, stop, ref, alt, p)
+  chrom = [(0, 2)]
+
+  #        012345678901234
+  #ref     ACTGACTGACTGACT
+  #alt     ACTG  TGACTGACT
+  #        0123  456789012
+  alt_seq, variant_waypoints = reads.expand_sequence(ref_seq, ml, chrom, 0)
+  read_list = np.rec.fromrecords([(n, 3) for n in range(6)], names=['start_a', 'read_len'])
+  cigars = reads.roll_cigars(variant_waypoints, read_list)
+  assert cigars[0] == '3M', cigars[0]
+  assert cigars[1] == '3M', cigars[1]
+  assert cigars[2] == '2M2D1M', cigars[2]
+  assert cigars[3] == '1M2D2M', cigars[3]
+  assert cigars[4] == '3M', cigars[4]
+  assert cigars[5] == '3M', cigars[5]
+
+
+def cigar_test6():
+  """Rolling cigars: SNP, INS, DEL in different combinations"""
+  #          012345678901234
+  ref_seq = 'ACTGACTGACTGACT'
+
+  #       0  1234567890  1234
+  #ref    A  CTGACTGACT  GACT
+  #alt    AAACGGA  GTCTTTGACT
+  #       0123456  7890123456
+  #           ^     ^
+  pos = [0, 2, 4, 8, 10]
+  stop = [1, 3, 7, 9, 11]
+  ref = ['A', 'T', 'ACT', 'A', 'T']
+  alt = ['AAA', 'G', 'A', 'T', 'TTT']
+  p = [0.1, .1, .1, .1, .1]
+  ml = vr.VariantList(pos, stop, ref, alt, p)
+  chrom = [(0, 2), (1, 2), (2, 2), (3, 2), (4, 2)]
+
+  alt_seq, variant_waypoints = reads.expand_sequence(ref_seq, ml, chrom, 0)
+  read_list = np.rec.fromrecords([(0, 3), (0, 4), (0, 5), (0, 7), (1, 7), (4, 8)],
+                                 names=['start_a', 'read_len'])
+  cigars = reads.roll_cigars(variant_waypoints, read_list)
+  assert cigars[0] == '1M2S', cigars[0]
+  assert cigars[1] == '1M2I1M', cigars[1]
+  assert cigars[2] == '1M2I1M1X', cigars[2]
+  assert cigars[3] == '1M2I1M1X2M', cigars[3]
+  assert cigars[4] == '2S1M1X2M2D1M', cigars[4]
+  assert cigars[5] == '1X2M2D1M1X2M1S', cigars[5]
