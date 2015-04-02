704111eac98aaf8e5f6320628b48df71f96fc9e7
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-04-02 15:31:49 -0400

    Rewrote for new version of reads

--- mitty/plugins/reads/simple_illumina_plugin.py
+++ mitty/plugins/reads/simple_illumina_plugin.py
@@ -5,11 +5,11 @@ Current contact: kaushik.ghose@sbgenomics.com
 """
 __example_param_text = """
 {
-  'read_len': 100,     #length of each read
-  'template_len': 250, #length of template
-  'coverage': 1.0,     #How many x coverage do we want
-  'max_p_error': 0.01,  #Maximum error rate at tip of read
-  'k': 0.3
+  'read_len': 100,          # length of each read
+  'template_len_mean': 250, # mean length of template
+  'template_len_sd': 30,    # sd of template length
+  'max_p_error': 0.01,      # Maximum error rate at tip of read
+  'k': 20                   # exponent
 }
 """
 
@@ -20,102 +20,105 @@ Example parameter set:
 
 _example_params = eval(__example_param_text)
 
-from mitty.lib.read import Read, direction
-import numpy
+import numpy as np
+
+import mitty.lib.util as mutil
+
 import logging
 logger = logging.getLogger(__name__)
 
 
-def initialize(model_params):
-  """The only interesting this we do here is setup the error profile."""
-  state = dict(model_params)
-  state['error_profile'] = [state['max_p_error'] * state['k'] ** n for n in range(state['read_len'])][::-1]
-  state['PHRED'] = ''.join([chr(int(33 + max(0, min(-10*numpy.log10(p), 93)))) for p in state['error_profile']])
-  return state
+class Model:
+  def __init__(self, read_len=100, template_len_mean=250, template_len_sd=50, max_p_error=0.01, k=20):
+    """."""
+    self.read_len, self.template_len_mean, self.template_len_sd = read_len, template_len_mean, template_len_sd
+    l = np.linspace(0, 1, self.read_len)
+    self.error_profile = max_p_error * (np.exp(k*l) - 1)/(np.exp(k) - 1)
+    self.phred = ''.join([chr(int(33 + max(0, min(-10*np.log10(p), 93)))) for p in self.error_profile])
+
+  def get_reads(self, seq, seq_c, coverage, corrupt, seed=1):
+    """The main simulation calls this function.
+
+    :param seq:      forward sequence
+    :param seq_c:    complement of sequence
+    :param coverage: coverage
+    :param corrupt:  T/F whether we should compute corrupted read or not
+    :param seed:     random number generator seed
+    :return: reads, paired
+
+    reads is a numpy recarray with the following fields
+      'start_a'  -> start index on the seq
+      'read_len' -> length of the read
+      'rev_complement' ->  0 or 1, indicating if the read is from the forward or reverse strand
+      'perfect_read'     -> perfect read sequence
+      'corrupted_read'   -> corrupted read sequence
+
+    paired indicates if the reads are in pairs or not
+    """
+    p_read = coverage / float(self.read_len) # Per base probability of a read
+    template_loc_rng, read_order_rng, template_len_rng, error_loc_rng, base_choice_rng = mutil.initialize_rngs(seed, 5)
+
+    template_locs = np.array([x for x in mutil.place_poisson(template_loc_rng, p_read, len(seq)) if seq[x] != 'N'], dtype='i4')
+    # TODO, refactor this into mutil by having seq as an numpy array rather than string
+    # also change for variant plugins. Need to change io.py. Should be transparent (non breaking change) at io.py
+    template_lens = (template_len_rng.randn(template_locs.shape[0]) * self.template_len_sd + self.template_len_mean).astype('i4')
+    np.clip(template_lens, self.read_len, self.template_len_mean * 3, template_lens)
+    read_order = read_order_rng.randint(2, size=template_locs.shape[0])  # Which read comes first?
+
+    dtype = [('start_a', 'i4'), ('read_len', 'i4'), ('read_order', 'i1'),
+             ('perfect_reads', 'S' + str(self.read_len)), ('corrupt_reads', 'S' + str(self.read_len)),
+             ('phred', 'S' + str(self.read_len))]
+    reads = np.core.recarray(dtype=dtype, shape=2 * template_locs.shape[0])
+    reads['start_a'][::2] = template_locs
+    reads['start_a'][1::2] = template_locs + template_lens - self.read_len
+    reads['read_len'] = self.read_len
+    reads['read_order'][::2] = read_order[:]
+    reads['read_order'][1::2] = 1 - read_order[:]
+    r_start, r_len, r_o, pr = reads['start_a'], reads['read_len'], reads['read_order'], reads['perfect_reads']
+    for n in xrange(reads.shape[0]):
+      if r_o[n] == 0:  # Forward read
+        pr[n] = seq[r_start[n]:r_start[n] + r_len[n]]
+      else:  # Reverse strand read
+        pr[n] = seq_c[r_start[n]:r_start[n] + r_len[n]][::-1]
+    if corrupt:
+      self.corrupt_reads(reads, error_loc_rng, base_choice_rng)
+    return reads
+
+  def corrupt_reads(self, reads, error_loc_rng, base_choice_rng):
+    """Corrupt reads
+
+    :param reads:   with the start_a, read_len and read_order fields filled out
+    :return: Fill in corrupted reads in place
+    """
+    pr, cr, ph = reads['perfect_reads'], reads['corrupt_reads'], reads['phred']
+    p = error_loc_rng.rand(reads.shape[0], self.read_len)
+    idx = [np.where(p[n, :] < self.error_profile)[0] for n in xrange(reads.shape[0])]
+    idx_tot = 0
+    for n in xrange(reads.shape[0]):
+      idx_tot += idx[n].size
+    corrupt_bases = base_choice_rng.choice(['A','C','G','T'], size=idx_tot, replace=True, p=[.3, .2, .2, .3]).tostring()
+
+    offset = 0
+    for n in xrange(reads.shape[0]):
+      ph[n] = self.phred
+      #idx, = np.where(p[n, :] < self.error_profile)
+      #corrupt_bases = base_choice_rng.choice(['A','C','G','T'], size=idx.size, replace=True, p=[.3, .2, .2, .3]).tostring()
+      b = bytearray(pr[n])
+      for m, i in enumerate(idx[n]):
+        b[i] = corrupt_bases[offset +m]
+      cr[n] = str(b)
+      offset += idx[n].size
 
 
-def max_read_len(read_model_state):
-  return read_model_state['read_len']
-
-
-def overlap_len(read_model_state):
-  return read_model_state['template_len'] - 1
-
-
-def init_rngs(master_seed):
-  read_loc_rng_seed, read_order_rng_seed, error_loc_rng_seed, base_choice_rng_seed = \
-    numpy.random.RandomState(seed=master_seed).randint(100000000, size=4)
-  logger.debug('Used master seed to generate seeds {:d}, {:d}, {:d}, {:d}'.
-               format(read_loc_rng_seed, read_order_rng_seed, error_loc_rng_seed, base_choice_rng_seed))
-  return {
-    'loc_rng': numpy.random.RandomState(seed=read_loc_rng_seed).randint,
-    'order_rng': numpy.random.RandomState(seed=read_order_rng_seed).randint,
-    'error_loc_rng': numpy.random.RandomState(seed=error_loc_rng_seed).rand,
-    'base_choice_rng': numpy.random.RandomState(base_choice_rng_seed).choice
-  }
-
-
-def generate_reads(this_idx, this_seq_block, this_c_seq_block, this_arr, read_model_state, master_seed):
-  rngs = init_rngs(master_seed)
-  seqs = [this_seq_block, this_c_seq_block]  # Just package it nicely
-  template_count = 0.5 * read_model_state['coverage'] * len(this_seq_block) / (read_model_state['read_len'] * 2.0)
-  rl = read_model_state['read_len']
-  tl = read_model_state['template_len']
-  t_start = rngs['loc_rng'](low=0, high=len(this_seq_block) - tl, size=template_count)
-  read_order = rngs['order_rng'](2, size=template_count)
-
-  template_list = extract_reads(seqs, t_start, read_order, tl, rl)
-
-  if read_model_state['generate_corrupted_reads']:  # Corrupt the bases and fill out the corrupted_seq field
-    erp = read_model_state['error_profile']
-    rl = read_model_state['read_len']
-    tc = len(template_list)
-    idx = numpy.where(rngs['error_loc_rng'](tc * len(template_list[0]), rl) < erp)
-    corrupt_bases = rngs['base_choice_rng'](['A','C','G','T'], size=idx[0].size, replace=True, p=[.3, .2, .2, .3]).tostring()
-
-    fill_out_corrupt_bases(template_list, corrupt_bases, idx, read_model_state['PHRED'])
-
-  return template_list
-
-
-def extract_reads(seqs, t_start, read_order, tl, rl):
-  """Refactored out random variables to make testing easier."""
-  template_list = []
-  for n in range(t_start.size):
-    if 'N' in seqs[0][t_start[n]:t_start[n]+rl] or 'N' in seqs[1][t_start[n]+tl-rl:t_start[n]+tl]:
-      continue
-
-    r1 = Read(perfect_seq=seqs[0][t_start[n]:t_start[n]+rl], _start_idx=t_start[n], _stop_idx=t_start[n]+rl, direction='>')
-    r2 = Read(perfect_seq=seqs[1][t_start[n]+tl-rl:t_start[n]+tl][::-1], _start_idx=t_start[n]+tl-rl, _stop_idx=t_start[n]+tl, direction='<')
-    if read_order[n]:
-      template_list += [[r2, r1]]
-    else:
-      template_list += [[r1, r2]]
-  return template_list
-
-
-def fill_out_corrupt_bases(template_list, corrupt_bases, idx, phred):
-  """Refactored out random variables to make testing easier."""
-  rl = len(template_list[0][0].perfect_seq)
-  r_phred = phred[::-1]
-
-  idx_ptr = 0
-  read_ctr = 0
-  for template in template_list:
-    for read in template:
-      read.PHRED = phred if read.direction == '>' else r_phred
-      if (idx_ptr == idx[0].size or  # We are out of read corruptions
-          idx[0][idx_ptr] < read_ctr):  # or this read is not corrupted
-        read.corrupt_seq = read.perfect_seq
-      else:
-        corrupt_seq = bytearray(read.perfect_seq)
-        while idx[0][idx_ptr] == read_ctr:
-          corrupt_seq[idx[1][idx_ptr]] = corrupt_bases[idx_ptr]  # Sequence ends always correspond to inner
-          idx_ptr += 1
-          if idx_ptr == idx[0].size:
-            break
-        read.corrupt_seq = corrupt_seq.__str__()
-      read_ctr += 1
+def self_test():
+  """Basic self test"""
+  import string
+  DNA_complement = string.maketrans('ATCGN', 'TAGCN')
+  seq = 'ATGTCGCCGGGCGCCATGCGTGCCGTTGTTCCCATTATCCCATTCCTTTTGGTTCTTGTCGGTGTATCGGGGGTTCCCACCAACGTCTCCTCCACCACCCAACCCCAACTCCAGACCACCGGTCGTCCCTCGCATGAAGCCCCCAACATGACCCAGACCGGCACCACCGACTCTCCCACCGCCATCAGCCTTACCACGCCCGACCACACACCCCCCATGCCAAGTATCGGACTGGAGGAGGAGGAAGAGGAGGAGGGGGCCGGGGATGGCGAACATCTTGAGGGGGGAGATGGGACCCGTGACACCCTACCCCAGTCCCCGGGTCCAGCCGTCCCGTTGGCCGGGGATGACGAGAAGGACAAACCCAACCGTCCCGTAGTCCCACCCCCCGGTCCCAACAACTCCCCCGCGCGCCCCGAGACCAGTCGACCGAAGACACCCCCCACCAGTATCGGGCCGCTGGCAACTCGACCCACGACCCAACTCCCCTCAAAGGGGCGACCCTTGGTTCCGACGCCTCAACATACCCCGCTGTTCTCGTTCCTCACTGCCTCCCCCGCCCTGGACACCCTCTTCGTCGTCAGCACCGTCATCCACACCTTATCGTTTTTGTGTATTGTTGCGATGGCGACACACCTGTGTGGCGGTTGGTCCAGACGCGGGCGACGCACACACCCTAGCGTGCGTTACGTGTGCCTGCCGCCCGAACGCGGGTAG'
+  seq_c = string.translate(seq, DNA_complement)
+  mdl = Model(4, 8, 2, max_p_error=1)
+  rd = mdl.get_reads(seq, seq_c, .00001, True)
+  assert type(rd) == np.core.records.recarray  # Basically, the previous code should just run
 
 
 if __name__ == "__main__":
