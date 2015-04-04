ed0ce98bc8c8014794da5bd858915fffcb5ef2e3
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-04-04 12:42:50 -0400

    read example updated

--- examples/reads/read_test.sh
+++ examples/reads/read_test.sh
@@ -1,40 +1,14 @@
 #!/bin/sh
 set -xe
 
-#pushd `dirname $0`
-
 : This shell script tests generating reads
-PROGDIR=../../mitty/
-GENOMEDIR=../data
-OUTDIR=Out/
+mkdir -p Out
 
-if [ $# -ne 1 ]
-then
-  echo "Arguments:"
-  echo "variants, nullseq, nullillumina, varillumina"
-  exit 0
-elif [ $1 == "variants" ]
-then
-  python ${PROGDIR}denovo.py --pfile=variants.json  -v
-  cat variants.vcf
-  exit 0
-elif [ $1 == "nullseq" ]
-then
-  PFILE=seq_reads.json
-elif [ $1 == "nullillumina" ]
-then
-  PFILE=null_illumina_reads.json
-elif  [ $1 == "varillumina" ]
-then
-  PFILE=var_illumina_reads.json
-else
-  exit 0
-fi
+: Generate null reads
+reads --pfile=null_illumina_reads.json -v
 
-mkdir -p ${OUTDIR}
+: Generate variant database
+genomes generate --pfile=variants.json -v
 
-: Create reads
-python ${PROGDIR}vcf2reads.py --pfile=${PFILE}  -V
-
-: Do the fake alignment to check
-python ${PROGDIR}reads2bam.py -p --fa_dir=${GENOMEDIR} --fastq ${OUTDIR}reads.fq --bam=${OUTDIR}reads_aligned.bam -v
+: Generate reads from variant sample
+reads --pfile=var_illumina_reads.json -v
