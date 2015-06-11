96645e0598903f020d9c80031ee20c388595e4fe
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-06-11 15:47:05 -0400

    Standardizing parameter names

--- mitty/plugins/variants/uniform_deletions.py
+++ mitty/plugins/variants/uniform_deletions.py
@@ -25,10 +25,10 @@ _example_params = eval(__example_param_text__)
 
 
 class Model:
-  def __init__(self, p=0.01, del_len_min=10, del_len_max=1000, **kwargs):
+  def __init__(self, p=0.01, min_len=10, max_len=1000, ref=None):
     assert 0 <= p <= 1.0, "Probability out of range"
-    assert 0 < del_len_min < del_len_max, "Check your del_len_min and del_len_max definitions"
-    self.p, self.del_len_min, self.del_len_max = p, del_len_min, del_len_max
+    assert 0 < min_len < max_len, "Check your min_len and max_len definitions"
+    self.p, self.del_len_min, self.del_len_max = p, min_len, max_len
 
   def get_variants(self, ref, p=None, f=None, seed=1, **kwargs):
     """This function is called by the simulator to obtain variants.
@@ -53,12 +53,12 @@ class Model:
     p_eff = scale_probability_and_validate(self.p, p, f)
     del_locs = mutil.place_poisson_seq(base_loc_rng, p_eff, 0, len(ref), ref)
     del_lens = del_len_rng.randint(low=self.del_len_min, high=self.del_len_max, size=del_locs.shape[0])
-    idx = ((del_locs + del_lens) < len(ref)).nonzero()[0]   # Get rid of any deletions that go past the sequence end
+    idx = ((del_locs + del_lens + 1) < len(ref)).nonzero()[0]   # Get rid of any deletions that go past the sequence end
     del_locs = del_locs[idx]
     del_lens = del_lens[idx]
     if len(del_locs):
       # http://stackoverflow.com/questions/8081545/convert-list-of-tuples-to-multiple-lists-in-python
-      idx, refs, alts = map(list, itertools.izip(*((n, ref[del_loc:del_loc + del_len], ref[del_loc]) for n, (del_loc, del_len) in enumerate(np.nditer([del_locs, del_lens])) if ref[del_loc + del_len - 1] != 'N')))
+      idx, refs, alts = map(list, itertools.izip(*((n, ref[del_loc:del_loc + del_len + 1], ref[del_loc]) for n, (del_loc, del_len) in enumerate(np.nditer([del_locs, del_lens])) if ref[del_loc + del_len - 1] != 'N')))
       # This gets rid of any deletions that stretch into the 'N' regions of a sequence
       del_locs, del_ends, p = del_locs[idx], del_locs[idx] + del_lens[idx], 1.0 - del_lens[idx] / float(del_lens[idx].max())
     else:
