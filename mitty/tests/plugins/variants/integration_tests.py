43f54a2c846dbe7624a62dedf4eae5bb9b46d31c
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-06-12 18:31:22 -0400

    Bugfix: variable name mixup in test

--- mitty/tests/plugins/variants/integration_tests.py
+++ mitty/tests/plugins/variants/integration_tests.py
@@ -84,12 +84,12 @@ def sanity_check_all_found_plugins_test():
 
 def variant_sanity_check(m):
   """Convenience function. Given an initialized model try and do a sanity check test with it."""
-  ref_seq = 'ACTG' * 1000
-  pos, stop, ref, alt, p = m.get_variants(ref_seq, seed=10)
+  ref_seq = ref[1]['seq']
+  pos, stop, refs, alts, p = m.get_variants(ref_seq, seed=10)
   if len(pos) == 0:
     raise SkipTest('The defaults do not yield any variants to test')
 
-  for p, s, r, a in zip(pos, stop, ref, alt):
+  for p, s, r, a in zip(pos, stop, refs, alts):
     assert r[0] == ref_seq[p]
     if len(r) != len(a):
       assert a[0] == ref_seq[p]
