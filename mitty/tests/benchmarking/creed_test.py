c4b1e273281ed836f1d3145c56d2a2c156076e28
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-07-09 17:58:46 -0400

    Adding tests for read analysis

--- mitty/tests/benchmarking/creed_test.py
+++ mitty/tests/benchmarking/creed_test.py
@@ -21,3 +21,43 @@ def categorized_reads_class_test():
   assert dta['pos'][0] < dta['pos'][1]
 
   assert_raises(RuntimeError, cat_reads.append, 1, 0, 10, 100, 0b10000)  # Can't append after finalizing
+
+
+class MyRead:
+  def __init__(self, qname, secondary, paired, read1, unmapped, reference_id, pos, cigarstring):
+    self.qname, self.is_secondary, self.is_paired, self.is_read1 = qname, secondary, paired, read1
+    self.is_unmapped, self.reference_id, self.pos, self.cigarstring = unmapped, reference_id, pos, cigarstring
+
+
+def analyze_read_test():
+  """Read analysis"""
+  qname = '3|15|0|1|898|100=|0|744|24=2I74='
+  read = MyRead(qname=qname, secondary=False, paired=True, read1=True, unmapped=False, reference_id=14, pos=898, cigarstring='100M')
+  read_serial, chrom, cpy, ro, pos, cigar, read_category = creed.analyze_read(read, window=0, extended=False)
+  assert read_serial == 30, read_serial
+  assert chrom == 15, chrom
+  assert cpy == 0, cpy
+  assert ro == 1, ro
+  assert pos == 898, pos
+  assert cigar == '100M', cigar
+  assert read_category == 0b010000, bin(read_category)
+
+  read = MyRead(qname=qname, secondary=False, paired=True, read1=False, unmapped=False, reference_id=14, pos=744, cigarstring='24M2I74M')
+  read_serial, chrom, cpy, ro, pos, cigar, read_category = creed.analyze_read(read, window=0, extended=False)
+  assert read_serial == 31, read_serial
+  assert chrom == 15, chrom
+  assert cpy == 0, cpy
+  assert ro == 0, ro
+  assert pos == 744, pos
+  assert cigar == '24M2I74M', cigar
+  assert read_category == 0b100000, bin(read_category)
+
+  read = MyRead(qname=qname, secondary=False, paired=True, read1=True, unmapped=False, reference_id=14, pos=898, cigarstring='24M2I74M')
+  read_serial, chrom, cpy, ro, pos, cigar, read_category = creed.analyze_read(read, window=0, extended=False)
+  assert read_serial == 30, read_serial
+  assert chrom == 15, chrom
+  assert cpy == 0, cpy
+  assert ro == 1, ro
+  assert pos == 898, pos
+  assert cigar == '100M', cigar
+  assert read_category == 0b010100, bin(read_category)
