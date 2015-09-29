810b27af246c7518a74f8ccd5eb12b2304847aa2
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-09-29 10:42:26 -0400

    Adding shorter genome for more convenient tutorials

---
+++ docs/tutorial/last_five_chromosomes.py
@@ -0,0 +1,21 @@
+"""For our tutorial we want to demonstrations on a genome that
+  * is not too big - don't want a ton of data to process each time we regnerate the docs
+  * want several chromosomes - important to illustrate all features
+  * is not too small - we want a decent number of variations and reads
+
+To this end we take the last five chromosomes of the red_alga genome and pretend these are chromosomes from a diploid
+creature 'Reddus pentalgus'"""
+import gzip
+
+input_fname = 'red_alga.fa.gz'
+output_fname = 'reddus_pentalgus.fa.gz'
+
+skip_cntr = 16
+with gzip.open(input_fname, 'r') as in_file, gzip.open(output_fname, 'w') as out_file:
+  while skip_cntr > 0:
+    ln = in_file.readline()
+    if ln.startswith('>'):
+      skip_cntr -= 1
+  out_file.write(ln)
+  for line in in_file:
+    out_file.write(line)
--- docs/tutorial/tutorial.sh
+++ docs/tutorial/tutorial.sh
@@ -2,11 +2,18 @@
 genomes generate variations.json -v -p
 reads generate reads.json -v -p
 
-bwa index red_alga.fa.gz
-pybwa red_alga.fa.gz reads_c.fq reads_c.bam -p -v
+#bwa index reddus_pentalgus.fa.gz
+pybwa reddus_pentalgus.fa.gz reads_c.fq reads_c.bam -p -v
 
 perfectbam reads_c.bam --catreads catreads.h5 -v --window 10 -p
-alindel catreads.h5 indel.pkl --vdb red_alga_genomes.h5 --sample_name g0_s0 --indel 50
+alindel catreads.h5 indel.pkl red_alga_genomes.h5 g0_s0 --indel-range 50
+
+pybwa red_alga.fa.gz reads_c.fq reads_c_2.bam -p -v -P -S  # No mate rescue or pairing
+
+perfectbam reads_c_2.bam --catreads catreads_2.h5 -v --window 10 -p
+alindel catreads_2.h5 indel_2.pkl red_alga_genomes.h5 g0_s0 --indel-range 50
+
+plot_indel -f indel.pkl -f indel_2.pkl -l 'BWA' -l 'BWA(bad)' --indel-range 50 --win 5
 
 #genomes write vcf Out/red_alga_genomes.h5 Out/alga  --sample_name g0_s0
 #alindel Out/catreads.h5 Out/indel2.pkl --vcf Out/alga_g0_s0.vcf.gz --indel 50
