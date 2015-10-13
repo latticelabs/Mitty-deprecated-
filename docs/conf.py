aa3c7e34e118cefdf85981083a8d4e7ee3bb5ee2
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-10-13 18:17:06 -0400

    Removing excludes as we work on them

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ release = __version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'additional_utils.rst', 'components.rst', 'developer.rst', 'file.rst', 'temp.rst']
+exclude_patterns = ['_build', 'additional_utils.rst', 'components.rst', 'files.rst', 'temp.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
