bdcf1c6210a7d3d626a85c6f79aeb51a82967c1f
Kaushik Ghose <kaushik.ghose@sbgenomics.com>
2015-10-23 08:45:58 -0400

    Code cleanup
    Removing deprecated functions that were being used to save alignment analysis results in custom formats (hdf5 and sqlite)

--- mitty/tests/benchmarking/creed_test.py
+++ mitty/tests/benchmarking/creed_test.py
@@ -3,26 +3,6 @@ from nose.tools import assert_raises
 import mitty.benchmarking.creed as creed
 
 
-def categorized_reads_class_test():
-  """benchmarking: CategorizedReads class"""
-  assert_raises(RuntimeError, creed.CategorizedReads)  # Need seq len and seq id for new file
-  seq_names = ['chr1', 'chr2']
-  seq_lengths = [100, 200]
-  cat_reads = creed.CategorizedReads(seq_names=seq_names, seq_lengths=seq_lengths, copies=2)
-  assert cat_reads.get_sequence_info()[1][1] == seq_lengths[1]
-
-  cat_reads.append(1, 0, 20, 100, 0b10010)
-  cat_reads.append(1, 0, 10, 100, 0b10000)
-  cat_reads.append(0, 1, 10, 100, 0b10000)
-  cat_reads.finalize()
-
-  dta = cat_reads.get_data(1, 0)
-  assert dta.shape[0] == 2
-  assert dta['pos'][0] < dta['pos'][1]
-
-  assert_raises(RuntimeError, cat_reads.append, 1, 0, 10, 100, 0b10000)  # Can't append after finalizing
-
-
 class MyRead:
   def __init__(self, qname, secondary, paired, read1, unmapped, reference_id, pos, cigarstring):
     self.qname, self.is_secondary, self.is_paired, self.is_read1 = qname, secondary, paired, read1
